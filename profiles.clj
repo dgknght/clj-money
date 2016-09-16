1f58e035cf307e5d6dc1da327f29ab7424f81134
Doug Knight <dougbknight@gmail.com>
2016-09-16 17:09:24 -0500

    add development and test env config

---
+++ profiles.clj
@@ -0,0 +1,6 @@
+{:dev
+ {:env
+  {:database-url "postgresql://localhost/5432/money_development"}}
+ :test
+ {:env
+  {:database-url "postgresql://localhost/5432/money_test"}}}
--- src/clj_money/db.clj
+++ src/clj_money/db.clj
@@ -1,6 +1,7 @@
 (ns clj-money.db
   (:require [ragtime.jdbc :as jdbc]
-            [ragtime.repl :as rt])
+            [ragtime.repl :as rt]
+            [environ.core :refer [env]])
   #_(:use [clj-money.config :as config]))
 
 #_(defn ragtime-config []
@@ -9,10 +10,10 @@
 
 (defn migrate
   []
-  (println "this is a migration")
+  (println (str "this is a migration for " (env :database-url)))
   #_(rt/migrate (ragtime-config)))
 
 (defn rollback
   []
-  (println "this is a rollback")
+  (println (str "this is a rollback for " (env :database-url)))
   #_(rt/rollback (ragtime-config)))
