fd3ef68572fb330d70ae979f212956feb062435c
Doug Knight <dougbknight@gmail.com>
2016-09-15 20:03:43 -0500

    correct accounts shared ref

--- src/clj_money/web/accounts.clj
+++ src/clj_money/web/accounts.clj
@@ -1,10 +1,11 @@
 (ns clj-money.web.accounts
   (:require [hiccup.core :refer :all]
-            [hiccup.page :refer :all]))
+            [hiccup.page :refer :all])
+  (:use [clj-money.web.shared :refer :all]))
 
 (defn index
   "Renders the list of accounts"
   []
-  (clj-money.web.pages/layout
+  (layout
     "Accounts" "Accounts"
     "Account list will go here."))
