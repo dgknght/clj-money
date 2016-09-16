ef28d065bd3a899be11c65116a8c5b9ccfa8f2df
Doug Knight <dougbknight@gmail.com>
2016-09-15 20:38:52 -0500

    working login

--- src/clj_money/web/pages.clj
+++ src/clj_money/web/pages.clj
@@ -1,6 +1,8 @@
 (ns clj-money.web.pages
   (:require [hiccup.core :refer :all]
-            [hiccup.page :refer :all])
+            [hiccup.page :refer :all]
+            [ring.util.response :refer :all]
+            [cemerick.friend.credentials :as creds])
   (:use [clj-money.web.shared :refer :all]))
 
 (defn home
@@ -12,19 +14,34 @@
             "Welcome to the accounting application"
             )))
 
-(def temp-model {:username "x"
-                 :errors {:username ["This is a test error."]}})
-
 (defn login
   "Renders the sign in form"
-  []
-  (layout
-    "Log in"
-    "Log in"
-    [:div.row
-     [:div.col-md-6
-      [:form {:action "/authenticate" :method :post}
-       (text-input-field temp-model :username)
-       (password-input-field temp-model :password)
-       [:input.btn.btn-primary {:type :submit :value "Log in"}]
-       [:a.btn.btn-default {:href "/"} "Cancel"]]]]))
+  ([] (login {:username nil :password nil}))
+  ([model]
+   (layout
+     "Log in"
+     "Log in"
+     [:div.row
+      [:div.col-md-6
+       [:form {:action "/authenticate" :method :post}
+        (text-input-field model :username {:autofocus true})
+        (password-input-field model :password)
+        [:input.btn.btn-primary {:type :submit :value "Log in"}]
+        [:a.btn.btn-default {:href "/"} "Cancel"]]]])))
+
+
+; TODO Replace this with a database implementation
+(def users
+  {"doug" {:username "doug"
+           :password (creds/hash-bcrypt "please01")
+           :roles #{::admin}}})
+
+(defn authenticate
+  "Verifies the supplied credentials"
+  [username password]
+  (if-let [user (get users username)]
+    (if (= (creds/hash-bcrypt password)
+           (:password user))
+      (redirect "/accounts") ; TODO Need the originally request url
+      (login {:username username :password password}))
+    (login {:username username :password password})))
--- src/clj_money/web/server.clj
+++ src/clj_money/web/server.clj
@@ -5,30 +5,35 @@
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
+            [ring.middleware.session :refer [wrap-session]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [environ.core :refer [env]]
             [cemerick.friend :as friend]
             [cemerick.friend.workflows :as workflows]
             [cemerick.friend.credentials :as creds])
-  (:use [clj-money.web.pages :as pages]))
+  (:use [clj-money.web.pages :as pages]
+        [clj-money.web.accounts :as accounts]))
 
 (defroutes app
   (GET "/" []
        (pages/home))
   (GET "/login" []
        (pages/login))
+  (GET "/authenticate" [username password]
+       (pages/authenticate username password))
+  (GET "/accounts" []
+       (friend/authorize #{::user} (accounts/index)))
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
-
-; TODO Replace this with a database implementation
-(def users
-  {"doug" {:username "doug"
-           :password (creds/hash-bcrypt "please01")
-           :roles #{::admin}}})
 
 (def secured-app
   (-> app
       (friend/authenticate {:credential-fn (partial creds/bcrypt-credential-fn users)
-                            :workflows [(workflows/interactive-form)]})))
+                            :workflows [(workflows/interactive-form)]})
+      (wrap-keyword-params)
+      (wrap-params)
+      (wrap-session)))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
