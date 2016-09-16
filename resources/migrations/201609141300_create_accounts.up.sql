5b47be50c9c33d6b5c53027fc750237bf8137793
Doug Knight <dougbknight@gmail.com>
2016-09-16 18:03:16 -0500

    move migrations where ragtime really wants them

--- resources/migrations/201609141300_create_accounts.up.sql
+++ resources/migrations/201609141300_create_accounts.up.sql
@@ -1,10 +1,11 @@
+create sequence accounts_id_seq
+  increment by 1
+  start 1;
+
 create table accounts (
   id int primary key not null default nextval('accounts_id_seq'),
   name varchar(50) not null
 );
 
-
-create sequence accounts_id_seq
-  increment by 1
-  start 1
+alter sequence accounts_id_seq
   owned by accounts.id;
