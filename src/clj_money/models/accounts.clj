041bbbe6e29ba41d351b72e63cf639867eaab028
Doug Knight <dougbknight@gmail.com>
2016-09-17 18:09:29 -0500

    make create-an-account test pass

--- src/clj_money/models/accounts.clj
+++ src/clj_money/models/accounts.clj
@@ -1,28 +1,20 @@
 (ns clj-money.models.accounts
   (:require [clojure.java.jdbc :as jdbc]
-            [clojure.pprint :as pp]
+            [clojure.pprint :refer [pprint]]
             [honeysql.core :as sql]
-            [honeysql.helpers :refer :all]))
+            [honeysql.helpers :as h]))
 
 (defn create
   "Creates a new account in the system"
   [data-store account]
-  (try
-    (let [sql (sql/format (-> (insert-into :accounts)
-                              (values [(update-in account [:type] name)])))]
+  (let [sql (sql/format (-> (h/insert-into :accounts)
+                            (h/values [(update-in account [:type] name)])))]
+    (jdbc/execute! data-store sql)))
 
-      (pp/pprint (str "sql: " sql))
-
-      (jdbc/execute! data-store sql))
-    (catch Exception e
-      (println "Unable to insert the account")
-      (pp/pprint account)
-      (pp/pprint (.getMessage (.getNextException e)))
-      [])))
-
-(defn list
+(defn select
   "Returns a list of all accounts in the system"
   [data-store]
-  (jdbc/query data-store
-              (sql/format (-> (select :*)
-                              (from :accounts)))))
+  (let [sql (sql/format (-> (h/select :*)
+                            (h/from :accounts)))]
+    (->> (jdbc/query data-store sql)
+         (map #(update-in % [:type] keyword)))))
--- test/clj_money/models/accounts_test.clj
+++ test/clj_money/models/accounts_test.clj
@@ -1,14 +1,19 @@
 (ns clj-money.models.accounts-test
   (:require [clojure.test :refer :all]
             [environ.core :refer [env]]
-            [clj-money.web :refer :all])
-  (:use [clj-money.models.accounts :as accounts])
-  )
+            [clj-money.web :refer :all]
+            [clojure.pprint :refer [pprint]]
+            [clojure.data :refer [diff]])
+  (:use [clj-money.models.accounts :as accounts]))
 
 (deftest create-an-account
   (testing "After I add an account, I can retrieve it"
     (accounts/create (env :db) {:name "Checking"
                                :type :asset})
-    (is (= [{:name "Checking"
-             :type :asset}]
-           (accounts/list (env :db))))))
+    (let [accounts (->> (env :db)
+                        (accounts/select)
+                        (map #(select-keys % [:name :type])))
+          expected [{:name "Checking"
+                     :type :asset}]]
+      (is (= expected
+             accounts)))))
