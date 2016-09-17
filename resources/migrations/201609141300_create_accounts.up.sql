58d633063ce1255441ca79d4719436c9c3f0ba6c
Doug Knight <dougbknight@gmail.com>
2016-09-17 01:28:44 -0500

    work accounts test

--- resources/migrations/201609141300_create_accounts.up.sql
+++ resources/migrations/201609141300_create_accounts.up.sql
@@ -4,8 +4,10 @@ create sequence accounts_id_seq
 
 create table accounts (
   id int primary key not null default nextval('accounts_id_seq'),
-  name varchar(50) not null
+  name varchar(50) not null,
+  type varchar(20) not null
 );
+create index uk_accounts_name on accounts (name);
 
 alter sequence accounts_id_seq
   owned by accounts.id;
--- src/clj_money/models/accounts.clj
+++ src/clj_money/models/accounts.clj
@@ -1,11 +1,28 @@
-(ns clj-money.models.accounts)
+(ns clj-money.models.accounts
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure.pprint :as pp]
+            [honeysql.core :as sql]
+            [honeysql.helpers :refer :all]))
 
 (defn create
   "Creates a new account in the system"
   [data-store account]
-  )
+  (try
+    (let [sql (sql/format (-> (insert-into :accounts)
+                              (values [(update-in account [:type] name)])))]
+
+      (pp/pprint (str "sql: " sql))
+
+      (jdbc/execute! data-store sql))
+    (catch Exception e
+      (println "Unable to insert the account")
+      (pp/pprint account)
+      (pp/pprint (.getMessage (.getNextException e)))
+      [])))
 
 (defn list
   "Returns a list of all accounts in the system"
   [data-store]
-  [])
+  (jdbc/query data-store
+              (sql/format (-> (select :*)
+                              (from :accounts)))))
