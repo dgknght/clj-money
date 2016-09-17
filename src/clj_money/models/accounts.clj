1eaf51faf43d3841f336f42ec4ab84df5c759ad4
Doug Knight <dougbknight@gmail.com>
2016-09-17 18:29:54 -0500

    extract data preparation functions

--- src/clj_money/models/accounts.clj
+++ src/clj_money/models/accounts.clj
@@ -4,12 +4,27 @@
             [honeysql.core :as sql]
             [honeysql.helpers :as h]))
 
+(defn prepare-accounts-for-save
+  "Adjusts account data for saving in the database"
+  [accounts]
+  ; convert account type from keyword to string
+  (map #(update-in % [:type] name)
+       accounts))
+
 (defn create
   "Creates a new account in the system"
   [data-store account]
-  (let [sql (sql/format (-> (h/insert-into :accounts)
-                            (h/values [(update-in account [:type] name)])))]
+  (let [values (prepare-accounts-for-save [account])
+        sql (sql/format (-> (h/insert-into :accounts)
+                            (h/values values)))]
     (jdbc/execute! data-store sql)))
+
+(defn prepare-account-data-from-db
+  "Adjusts account data read from the database for use"
+  [accounts]
+  ; change account type to a keyword
+  (map #(update-in % [:type] keyword)
+       accounts))
 
 (defn select
   "Returns a list of all accounts in the system"
@@ -17,4 +32,4 @@
   (let [sql (sql/format (-> (h/select :*)
                             (h/from :accounts)))]
     (->> (jdbc/query data-store sql)
-         (map #(update-in % [:type] keyword)))))
+         (prepare-account-data-from-db))))
