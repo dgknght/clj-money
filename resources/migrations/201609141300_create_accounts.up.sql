271d4e9ed0c2dd3ccb83aa76beeb0060c900c573
Doug Knight <dougbknight@gmail.com>
2016-09-17 19:00:44 -0500

    enforce unique account names

--- resources/migrations/201609141300_create_accounts.up.sql
+++ resources/migrations/201609141300_create_accounts.up.sql
@@ -7,7 +7,7 @@ create table accounts (
   name varchar(50) not null,
   type varchar(20) not null
 );
-create index uk_accounts_name on accounts (name);
+create unique index uk_accounts_name on accounts (name);
 
 alter sequence accounts_id_seq
   owned by accounts.id;
