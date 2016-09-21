d68f513f2eecfe4ec1b959e3bda36eaff4e0ccf2
Doug Knight <dougbknight@gmail.com>
2016-09-21 18:32:44 +0900

    refine logic for selecting a storage service

--- src/clj_money/models/helpers.clj
+++ src/clj_money/models/helpers.clj
@@ -3,9 +3,21 @@
   (:import clj_money.models.storage.sql_storage.SqlStorage))
 
 ; TODO should this be dynamic to support extension?
+(def handlers
+  [{:can-handle-fn #(= "postgresql" (:dbtype %))
+    :create-fn #(SqlStorage. %)}])
+
+(defn- process-handler
+  "Calls the specified check function to see if the handler
+  can handle the specified config. If yes, then the create
+  function is called to return the storage server. Otherwise
+  nil is returned"
+  [{:keys [can-handle-fn create-fn]} config]
+  (when (can-handle-fn config)
+    (create-fn config)))
+
 (defn storage
   "Returns a storage implementation appropriate
   for the specified config"
   [config]
-  (cond
-    (= "postgresql" (:dbtype config)) (SqlStorage. config)))
+  (some #(process-handler % config) handlers))
