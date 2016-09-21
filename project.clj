6a8477fbb8bbd15ceffac1bf77e15ece2c9d9217
Doug Knight <dougbknight@gmail.com>
2016-09-21 11:48:45 +0900

    add factory and faker libraries

--- project.clj
+++ project.clj
@@ -15,7 +15,9 @@
                  [environ "1.0.0"]
                  [com.cemerick/friend "0.2.3"]
                  [ragtime "0.6.3"]
-                 [prismatic/schema "1.1.3"]]
+                 [prismatic/schema "1.1.3"]
+                 [clj-factory "0.2.1"]
+                 [faker "0.2.2"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
