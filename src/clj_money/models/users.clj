ac68302edf49d73fd418b34cd68ba9032201d1de
Doug Knight <dougbknight@gmail.com>
2016-09-21 09:15:38 +0900

    create Storage protocol

--- src/clj_money/models/users.clj
+++ src/clj_money/models/users.clj
@@ -10,11 +10,58 @@
             [schema.coerce :as coerce]
             [schema.utils :as sutils]))
 
+(defprotocol Storage
+  "Provides data storage services for the application"
+  (create-user
+    [this user]
+    "Creates a new user record")
+  (select-users
+    [this]
+    "Returns all of the users in the system matching the specified criteria")
+  (find-user-by-email
+    [this email]
+    "Returns the user having the specified email"))
+
+(deftype SqlStorage [db-spec]
+  Storage
+
+  (create-user
+    [_ user]
+    (try
+      (->> user
+           (jdbc/insert! db-spec :users)
+           first)
+      (catch java.sql.BatchUpdateException e
+        (log/error (format "Unable to insert user %s: %s"
+                        user
+                        (.getMessage (.getNextException e))))
+        (throw e))))
+
+  (select-users
+    [_]
+    (let [sql (sql/format (-> (h/select :first_name :last_name :email)
+                              (h/from :users)))]
+      (jdbc/query db-spec sql)))
+
+  (find-user-by-email
+    [this email]
+    (let [sql (sql/format (-> (h/select :id :first_name :last_name :email :password)
+                              (h/from :users)
+                              (h/where [:= :email email])))]
+      (->> sql
+           (jdbc/query db-spec)
+           first))))
+
 (defn prepare-user-for-insertion
   "Prepares a user record to be saved in the database"
   [user]
   (-> user
       (update-in [:password] hash-bcrypt)))
+
+(defn prepare-user-for-return
+  "Prepares a user record for return to the caller"
+  [user]
+  (dissoc user :password))
 
 (def EmailPattern #"\A[\w\.-_]+@[\w\.-_]+\.\w{2,4}\z")
 
@@ -50,35 +97,23 @@
 (defn create
   "Creates a new user record"
   [data-store user]
-  (let [user (validate-new-user user)]
-    (try
-      (dissoc (->> user
-                   prepare-user-for-insertion
-                   (jdbc/insert! data-store :users)
-                   first)
-              :password)
-      (catch java.sql.BatchUpdateException e
-        (log/error (str "Unable to insert user "
-                        user
-                        ": "
-                        (.getMessage (.getNextException e))))))))
+  (->> user
+       validate-new-user
+       prepare-user-for-insertion
+       (create-user (SqlStorage. data-store))
+       prepare-user-for-return))
 
 (defn select
   "Lists the users in the database"
   [data-store]
-  (let [sql (sql/format (-> (h/select :first_name :last_name :email)
-                            (h/from :users)))]
-    (jdbc/query data-store sql)))
+  (select-users (SqlStorage. data-store)))
 
 (defn authenticate
   "Returns the user with the specified username and password.
   The returned map contains the information cemerick friend
   needs to operate"
   [data-store {:keys [username password]}]
-  (let [sql (sql/format (-> (h/select :id :first_name :last_name :email :password)
-                            (h/from :users)
-                            (h/where [:= :email username])))
-        user (first (jdbc/query data-store sql))]
+  (let [user (find-user-by-email (SqlStorage. data-store) username)]
     (when (and user (bcrypt-verify password (:password user)))
       (-> user
           (dissoc :password)
