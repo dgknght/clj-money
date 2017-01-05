5636c943774afc74dbf0f21a8af3bc73e445ae5d
Doug Knight <doug.knight@livingsocial.com>
2017-01-05 15:59:51 -0600

    remove unneeded helper functions

--- src/clj_money/models/helpers.clj
+++ src/clj_money/models/helpers.clj
@@ -1,8 +1,5 @@
 (ns clj-money.models.helpers
   (:require [clojure.pprint :refer [pprint]]
-            [schema.core :as schema]
-            [schema.coerce :as coerce]
-            [schema.utils :as sutils]
             [clj-money.models.storage :refer [with-transaction]]
             [clj-money.models.storage.sql-storage])
   (:import clj_money.models.storage.sql_storage.SqlStorage
@@ -56,44 +53,3 @@
   `(let [s# (storage* ~(second binding))
          f# (fn* [~(first binding)] ~@body)]
      (with-transaction s# f#)))
-
-;; Validation
-(defn- int-matcher
-  [schema]
-  (when (= schema/Int schema)
-    (coerce/safe
-      (fn [value]
-        (if (and (string? value) (re-matches #"\d+" value))
-          (Integer. value)
-          value)))))
-
-(defn- nil-matcher
-  [schema]
-  (when (= schema/Str schema)
-    (coerce/safe
-      (fn [value]
-        (if (and (string? value) (= 0 (count value)))
-          nil
-          value)))))
-
-(defn throw-validation-exception
-  "Throws a validation exception that is consistent with
-  what is thrown by prismatic schema"
-  [errors model schema model-name]
-  (throw (ex-info (format "The %s is not valid." model-name)
-                      (assoc {:schema schema
-                              :value model
-                              :type :schema.core/error}
-                             :error errors))))
-
-(defn validate-model
-  "Coerces and validates the specified model using the specified schema"
-  [model schema model-name]
-  (let [coercer (coerce/coercer schema
-                                (coerce/first-matcher [int-matcher
-                                                       nil-matcher
-                                                       coerce/json-coercion-matcher]))
-        result (coercer model)]
-    (if (sutils/error? result)
-     (throw-validation-exception (:error result) model schema model-name)
-      result)))
