412559d66dc9f0c2d002d95462fe9ed2f2cc885e
Doug Knight <doug.knight@livingsocial.com>
2017-01-26 13:17:34 -0600

    refine integer collection coercion

--- src/clj_money/coercion.clj
+++ src/clj_money/coercion.clj
@@ -19,6 +19,32 @@
   [rule-key & paths]
   (map #(rule rule-key %) paths))
 
+(defmulti parse-integer-collection
+  (fn [value]
+    (cond
+      (string? value) :string
+      (and (coll? value) (every? integer? value)) :integer-collection
+      (coll? value) :collection)))
+
+(defmethod parse-integer-collection :string
+  [value]
+  [(try
+     (Integer. value)
+     (catch NumberFormatException e
+       value))])
+
+(defmethod parse-integer-collection :integer-collection
+  [value]
+  value)
+
+(defmethod parse-integer-collection :collection
+  [value]
+  (map #(try
+          (Integer. %)
+          (catch NumberFormatException e
+            %))
+       value))
+
 (register-coerce-fn :integer    #(if (integer? %) % (Integer. %)))
 (register-coerce-fn :decimal    #(if (decimal? %) % (try
                                                       (bigdec %)
@@ -26,14 +52,7 @@
                                                         %))))
 (register-coerce-fn :keyword    #(if (keyword? %) % (keyword %)))
 (register-coerce-fn :local-date #(if (string? %) (parse-local-date %) %))
-(register-coerce-fn :integer-collection #(if (and (seq %) (every? (fn [v] (integer? v)) %))
-                                           %
-                                           (mapv (fn [value]
-                                                   (try
-                                                     (Integer. value)
-                                                     (catch IllegalArgumentException e
-                                                       value)))
-                                                 %)))
+(register-coerce-fn :integer-collection parse-integer-collection)
 
 (defn coerce
   "Given a model and a list of coercion rules, applies the rules to the model"
