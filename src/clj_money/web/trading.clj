da5762189cef7c00a6fe23989c3d0027ca3774dc
Doug Knight <dougbknight@gmail.com>
2017-04-18 18:48:31 -0500

    add sale form

--- src/clj_money/web/trading.clj
+++ src/clj_money/web/trading.clj
@@ -27,37 +27,47 @@
                 (format "%s (%s)" (:name %) (:symbol %)))
        (commodities/select-by-entity-id (env :db) entity-id)))
 
+(defn- form-fields
+  [account trade]
+  (html
+    (date-input-field trade
+                      :trade-date
+                      {:autofocus true})
+    (select-field trade
+                  :commodity-id
+                  (commodity-options (:entity-id account)))
+    (number-input-field trade :shares)
+    (number-input-field trade :value)
+    [:input.btn.btn-primary {:type :submit
+                             :value "Submit"
+                             :title "Click here to save this trade."}]
+    "&nbsp;"
+    [:a.btn.btn-default {:href (format "/accounts/%s" (:id account))
+                         :title "Click here to go back to the account page."}
+     "Back"]))
+
+(defn- params->trade
+  [params]
+  (cond-> {:trade-date (t/today)}
+    (:commodity-id params)
+    (assoc :commodity-id
+           (Integer. (:commodity-id params)))
+
+    (:shares params)
+    (assoc :shares
+           (bigdec (:shares params)))))
+
 (defn new-purchase
   "Renders the form that allows the user to record a commodity purchase"
   ([{params :params :as req}]
-   (new-purchase req (cond-> {:trade-date (t/today)}
-                       (:commodity-id params)
-                       (assoc :commodity-id
-                              (Integer. (:commodity-id params)))
-
-                       (:shares params)
-                       (assoc :shares
-                              (bigdec (:shares params))))))
+   (new-purchase req (params->trade params)))
   ([{params :params} purchase]
    (let [account (accounts/find-by-id (env :db) (Integer. (:account-id params)))]
      (with-trading-layout "New Purchase" (:entity-id account) {}
        [:div.col-md-6
         [:form {:action (format "/accounts/%s/purchases" (:id account))
                 :method :post}
-         (date-input-field purchase
-                           :trade-date)
-         (select-field purchase
-                       :commodity-id
-                       (commodity-options (:entity-id account)))
-         (number-input-field purchase :shares)
-         (number-input-field purchase :value)
-         [:input.btn.btn-primary {:type :submit
-                                  :value "Submit"
-                                  :title "Click here to save this purchase."}]
-         "&nbsp;"
-         [:a.btn.btn-default {:href (format "/accounts/%s" (:id account))
-                              :title "Click here to go back to the account page."}
-          "Back"]]]))))
+         (form-fields account purchase)]]))))
 
 (defn purchase
   [{params :params :as req}]
@@ -71,8 +81,15 @@
       (redirect (format "/accounts/%s" (:account-id result))))))
 
 (defn new-sale
-  [req]
-  "new-sale")
+  ([{params :params :as req}]
+   (new-sale req (params->trade params)))
+  ([{params :params} sale]
+   (let [account (accounts/find-by-id (env :db) (Integer. (:account-id params)))]
+     (with-trading-layout "New Sale" (:entity-id account) {}
+       [:div.col-md-6
+        [:form {:action (format "/accounts/%s/sales" (:id account))
+                :method :post}
+         (form-fields account sale)]]))))
 
 (defn sell
   [req]
