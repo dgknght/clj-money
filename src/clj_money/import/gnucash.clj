cf307c60222dca1097b0573a9b2da9ff391a4309
Doug Knight <dougbknight@gmail.com>
2017-05-11 15:40:45 -0700

    use xpath for selective element processing

--- src/clj_money/import/gnucash.clj
+++ src/clj_money/import/gnucash.clj
@@ -3,54 +3,27 @@
   (:require [clojure.java.io :as io]
             [clojure.pprint :refer [pprint]]
             [clojure.set :refer [rename-keys]]
-            [clojure.data.xml :as xml]
+            [clj-xpath.core :refer :all]
             [clj-money.util :refer [pprint-and-return]]
             [clj-money.import :refer [read-source]])
   (:import java.util.zip.GZIPInputStream))
 
 (defmulti process-node
   (fn [_ node]
-    (if (string? node)
-      :string
-      (:tag node))))
+    (:tag node)))
 
-(defmethod process-node :string
-  [callback node]
-  ; no action required
-  )
+(defn- process-node-attribute
+  [node result {:keys [attribute xpath transform-fn]}]
+  (let [transform-fn (if transform-fn
+                       transform-fn
+                       identity)
+        raw-value ($x:text? xpath node)
+        value (transform-fn raw-value)]
+    (assoc result attribute value)))
 
-(defmethod process-node :xmlns.http%3A%2F%2Fwww.gnucash.org%2FXML%2Fgnc/count-data
-  [callback node]
-  #_(pprint {:count-data node}))
-
-(defmethod process-node :xmlns.http%3A%2F%2Fwww.gnucash.org%2FXML%2Fgnc/book
-  [callback node]
-  ; TODO Maybe this is an entity?
-  (doseq [node (:content node)]
-    (process-node callback node)))
-
-(defmethod process-node :xmlns.http%3A%2F%2Fwww.gnucash.org%2FXML%2Fbook/id
-  [callback node]
-  ; can we ignore this?
-  )
-
-(defn- node->map
-  [node]
-  (->> node
-       :content
-       (remove string?)
-       (reduce #(assoc %1 (:tag %2) (-> %2 :content first)) {})))
-
-(defmethod process-node :xmlns.http%3A%2F%2Fwww.gnucash.org%2FXML%2Fgnc/commodity
-  [callback node]
-  ; TODO come back and get this later
-  #_(pprint {:commodity node}))
-
-(def ^:private account-attributes-map
-  {:xmlns.http%3A%2F%2Fwww.gnucash.org%2FXML%2Fact/id :id
-   :xmlns.http%3A%2F%2Fwww.gnucash.org%2FXML%2Fact/name :name
-   :xmlns.http%3A%2F%2Fwww.gnucash.org%2FXML%2Fact/type :type
-   :xmlns.http%3A%2F%2Fwww.gnucash.org%2FXML%2Fact/parent :parent-id})
+(defn- node->model
+  [node attributes]
+  (reduce (partial process-node-attribute node) {} attributes))
 
 (def ^:private account-types-map
   {"ASSET"      :asset
@@ -60,28 +33,33 @@
    "LIABILITY"  :liability
    "CREDIT"     :liability})
 
-(defn- node->account
-  [node]
-  (-> node
-      node->map
-      (rename-keys account-attributes-map)
-      (select-keys (vals account-attributes-map))
-      (update-in [:type] account-types-map)))
+(def ^:private account-attributes
+  [{:attribute :name
+    :xpath "act:name"}
+   {:attribute :type
+    :xpath "act:type"
+    :transform-fn account-types-map}
+   {:attribute :id
+    :xpath "act:id"}
+   {:attribute :parent-id
+    :xpath "act:parent"}])
 
-(defmethod process-node :xmlns.http%3A%2F%2Fwww.gnucash.org%2FXML%2Fgnc/account
+(defmethod process-node :gnc:account
   [callback node]
-  (let [account (node->account node)]
+  (let [account (node->model node account-attributes)]
     (when (:type account)
       ((.account callback) account))))
 
-(defmethod process-node :xmlns.http%3A%2F%2Fwww.gnucash.org%2FXML%2Fgnc/transaction
-  [callback node]
-  #_(println "transaction"))
+(def ^:private namespace-map
+  {"gnc" "http://www.gnucash.org/XML/gnc"
+   "act" "http://www.gnucash.org/XML/act"})
 
 (defmethod read-source :gnucash
   [_ input callback]
-  (let [root (-> (GZIPInputStream. input)
-                 io/reader
-                 xml/parse)]
-    (doseq [node (:content root)]
-      (process-node callback node))))
+  (with-namespace-context namespace-map
+    (let [xml (->> (GZIPInputStream. input)
+                   io/reader
+                   slurp
+                   xml->doc)]
+      (doseq [node ($x "/gnc-v2/gnc:book/gnc:account" xml)]
+        (process-node callback node)))))
