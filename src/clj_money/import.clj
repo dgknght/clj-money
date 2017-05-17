342cd56c86d8132c7cd76d9d0216f8534f75d26a
Doug Knight <dougbknight@gmail.com>
2017-05-17 09:44:07 -0500

    readability changes

--- src/clj_money/import.clj
+++ src/clj_money/import.clj
@@ -1,7 +1,9 @@
 (ns clj-money.import
   (:refer-clojure :exclude [update])
   (:require [clojure.pprint :refer [pprint]]
-            [clj-money.util :refer [pprint-and-return-l]]
+            [clj-money.util :refer [pprint-and-return
+                                    pprint-and-return-l]]
+            [clj-money.validation :as validation]
             [clj-money.models.users :as users]
             [clj-money.models.entities :as entities]
             [clj-money.models.accounts :as accounts]
@@ -21,7 +23,7 @@
         parent-id (get-in context [:accounts original-parent-id])
         to-create (cond-> account
                     true
-                    (assoc :entity-id (:entity-id context))
+                    (assoc :entity-id (-> context :entity :id))
 
                     true
                     (dissoc :id)
@@ -29,18 +31,34 @@
                     parent-id
                     (assoc :parent-id parent-id))
         result (accounts/create (:storage context) to-create)]
+    (when-not (:id result)
+      (throw (ex-info (str
+                        "Unable to create the account "
+                        (validation/error-messages result))
+                      {:result result})))
     (update-in context [:accounts] #(assoc % original-id (:id result)))))
+
+(defn- resolve-account-references
+  [context items]
+  (map (fn [item]
+         (update-in item [:account-id] #(-> context :accounts (get %))))
+       items))
+
+(defn- prepare-transaction
+  [context transaction]
+  (-> transaction
+      (update-in [:items] #(resolve-account-references context %))
+      (assoc :entity-id (-> context :entity :id))))
 
 (defn- import-transaction
   [context transaction]
-  (let [to-create (-> transaction
-                      (update-in [:items] (fn [items]
-                                            (map (fn [item]
-                                                   (update-in item [:account-id] #(-> context :accounts (get %))))
-                                                 items)))
-                      (assoc :entity-id (:entity-id context)))]
-    (transactions/create (:storage context) to-create)
-    context)) ; Update anything in the context? don't want to include all transactions, as that can be many
+  (->> transaction
+       (prepare-transaction context)
+       (transactions/create (:storage context)))
+  ; Update anything in the context?
+  ; don't want to include all transactions,
+  ; as that can be many
+  context)
 
 (defn import-data
   "Reads the contents from the specified input and saves
@@ -49,9 +67,11 @@
   is created"
   [storage-spec user entity-name input source-type]
   (with-transacted-storage [s storage-spec]
-    (let [entity (entities/find-or-create s user  entity-name)
-          context (atom {:storage s
+    (let [context (atom {:storage s
                          :accounts {}
-                         :entity-id (:id entity)})]
-      (read-source source-type input (->Callback (fn [account] (swap! context #(import-account % account)))
-                                                 (fn [transaction] (swap! context #(import-transaction % transaction))))))))
+                         :entity (entities/find-or-create s user entity-name)})
+          callback (->Callback (fn [account]
+                                 (swap! context #(import-account % account)))
+                               (fn [transaction]
+                                 (swap! context #(import-transaction % transaction))))]
+      (read-source source-type input callback))))
