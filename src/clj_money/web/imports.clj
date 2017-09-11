7e1b93d78e3734b1b7f8b622ec2a35eb24c0acec
Doug Knight <dougbknight@gmail.com>
2017-09-11 14:20:36 -0500

    special anti-forgery handling for import
    [Fixes #150946520]

--- src/clj_money/web/imports.clj
+++ src/clj_money/web/imports.clj
@@ -3,6 +3,7 @@
   (:require [clojure.pprint :refer [pprint]]
             [environ.core :refer [env]]
             [ring.util.response :refer :all]
+            [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]
             [hiccup.core :refer :all]
             [hiccup.page :refer :all]
             [clj-money.web.shared :refer :all]))
@@ -13,7 +14,9 @@
     [:script {:src "/js/angular.min.js"}]
     [:script {:src "/js/underscore-min.js"}]
     [:script {:src "/js/import.js"}]
-    [:div {:ng-app "clj-money-import" :ng-controller "ImportController"}
+    [:div {:ng-app "clj-money-import"
+           :ng-controller "ImportController"
+           :ng-init (format "antiForgeryToken = '%s';" *anti-forgery-token*)}
      [:div.row {:ng-hide "alerts.length == 0 && statusMessage == null"}
       [:div.col-md-12
        [:div.alert
@@ -25,20 +28,20 @@
         "{{ statusMessage }}"]]]
      [:div.row {:ng-hide "activeImport"}
       [:div.col-md-6
-       (form "/entities/import" {}
-             [:div.form-group
-              [:label.control-label {:for "entity-name"} "Name"]
-              [:input.form-control {:type :text
-                                    :name "entity-name"
-                                    :id "entity-name"
-                                    :ng-model "entityName"
-                                    :autofocus true}]]
-             [:div.form-group
-              [:label.control-label {:for "source-file"} "Source file"]
-              [:input.form-control {:type :file
-                                    :file-model "sourceFile"
-                                    :name "source-file"
-                                    :id "source-file"}]])
+       [:form
+        [:div.form-group
+         [:label.control-label {:for "entity-name"} "Name"]
+         [:input.form-control {:type :text
+                               :name "entity-name"
+                               :id "entity-name"
+                               :ng-model "entityName"
+                               :autofocus true}]]
+        [:div.form-group
+         [:label.control-label {:for "source-file"} "Source file"]
+         [:input.form-control {:type :file
+                               :file-model "sourceFile"
+                               :name "source-file"
+                               :id "source-file"}]]]
        [:button.btn.btn-primary
         {:title "Click here to upload the specified file and import the data into a new entity."
          :ng-click "startImport();"}
