512fc1ac20524f2b2836ffc500d7cf984695b670
Doug Knight <dougbknight@gmail.com>
2017-10-13 22:03:21 -0500

    correct grants delete test

--- test/clj_money/models/grants_test.clj
+++ test/clj_money/models/grants_test.clj
@@ -79,7 +79,7 @@
         entity (find-entity context "Business")
         user (find-user context "jane@doe.com")
         grant (find-grant context (:id entity) (:id user))
-        _ (grants/delete storage-spec grant)
+        _ (grants/delete storage-spec (:id grant))
         grant-list (grants/search storage-spec {:entity-id (:id entity)})]
     (is (empty? (filter #(= (:id grant) (:id %)) grant-list))
         "The grant is not present after delete")))
