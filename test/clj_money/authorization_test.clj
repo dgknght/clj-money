a526cf57ade936777ab66b6e033f146b60737b6b
Doug Knight <dougbknight@gmail.com>
2017-10-15 20:19:34 -0500

    move account permission tests into a sep. namespace

--- test/clj_money/authorization_test.clj
+++ test/clj_money/authorization_test.clj
@@ -82,88 +82,17 @@
         (is (not (allowed? :create entity))
             "Create is not allowed")))))
 
-(def accounts-context
-  (assoc entities-context :accounts [{:name "Checking"
-                                      :type :asset
-                                      :entity-id "Personal"}
-                                     {:name "Savings"
-                                      :type :asset
-                                      :entity-id "Business"}]))
-
-(deftest account-list
-  (let [context (serialization/realize storage-spec accounts-context)
-        [john jane] (find-users context "john@doe.com" "jane@doe.com")
-        entity (find-entity context "Personal")]
-    (testing "A user has permission to list accounts in his entities"
-      (with-authentication john
-        (is (not= 0 (->> (apply-scope{:entity-id (:id entity)} :account)
-                         (accounts/search storage-spec)
-                         count))
-            "The accounts are returned")))
-    (testing "A user does not have permission list accounts in someone else's entity"
-      (with-authentication jane
-        (is (thrown+? [:type :clj-money.authorization/unauthorized]
-                     (->> (apply-scope{:entity-id (:id entity)} :account)
-                          (accounts/search storage-spec)
-                          count)))))
-    (testing "A user can be granted permission to list accounts in someone else's entity"
-      (grants/create storage-spec {:entity-id (:id entity)
-                                   :user-id (:id jane)
-                                   :permissions {:account #{:index}}})
-      (with-authentication jane
-        (is (not= 0 (->> (apply-scope{:entity-id (:id entity)} :account)
-                         (accounts/search storage-spec)
-                         count))
-            "The accounts are returned")))))
-
-(deftest account-management
-  (let [context (serialization/realize storage-spec accounts-context)
-        [john jane] (find-users context "john@doe.com" "jane@doe.com")
-        checking (find-account context "Checking")]
-    (testing "A user has permission on accounts in his own entities"
-      (with-authentication john
-        (doseq [action [:show :edit :update :delete]]
-          (is (allowed? action checking)
-              (format "A user has %s permission" action)))))
-    (testing "A user does not have permission on accounts in someone else's entity"
-      (with-authentication jane
-        (doseq [action [:show :edit :update :delete]]
-          (is (not (allowed? action checking))
-              (format "A user does not have %s permission" action)))))
-    (testing "A user can be granted permission on accounts in someone else's entity"
-      (with-authentication jane
-        (grants/create storage-spec {:user-id (:id jane)
-                                     :entity-id (:entity-id checking)
-                                     :permissions {:account #{:show}} })
-        (doseq [action [:show]]
-          (is (allowed? action checking)
-              (format "A user has %s permission" action)))
-        (doseq [action [:edit :update :delete]]
-          (is (not (allowed? action checking))
-              (format "A user does not have %s permission" action)))))))
-
-(deftest account-creation
-  (let [context (serialization/realize storage-spec accounts-context)
-        [john jane] (find-users context "john@doe.com" "jane@doe.com")
-        personal (find-entity context "Personal")
-        savings (tag-resource {:name "Salary"
-                               :type :income
-                               :entity-id (:id personal)}
-                              :account)]
-    (testing "A user has permission to create an account in his own entities"
-      (with-authentication john
-        (is (allowed? :create savings)
-            "Create is allowed")))
-    (testing "A user does not have permission to create an account in someone else's entities"
-      (with-authentication jane
-        (is (not (allowed? :create savings))
-            "Create is not allowed")))))
-
 (def transactions-context
-  (-> accounts-context
-      (update-in [:accounts] #(conj % {:name "Salary"
-                                       :type :income
-                                       :entity-id "Personal"}))
+  (-> entities-context
+      (assoc :accounts [{:name "Checking"
+                         :type :asset
+                         :entity-id "Personal"}
+                        {:name "Savings"
+                         :type :asset
+                         :entity-id "Business"}
+                        {:name "Salary"
+                         :type :income
+                         :entity-id "Personal"}])
       (assoc :transactions [{:transaction-date (t/local-date 2017 3 2)
                              :description "Paycheck"
                              :entity-id "Personal"
@@ -230,7 +159,7 @@
             "Create is not allowed")))))
 
 (def ^:private budgets-context
-  (-> accounts-context
+  (-> entities-context
       (assoc :budgets [{:name "2017"
                         :entity-id "Personal"
                         :start-date (t/local-date 2017 1 1)
@@ -239,8 +168,14 @@
                         :items [{:account-id "Income"
                                  :periods (repeat 12 100M)
                                  :start-date (t/local-date 2018 1 1)}]}])
-      (update-in [:accounts] #(conj % {:type :income
-                                     :name "Income"}))))
+      (assoc :accounts [{:name "Checking"
+                         :type :asset
+                         :entity-id "Personal"}
+                        {:name "Savings"
+                         :type :asset
+                         :entity-id "Business"}
+                        {:type :income
+                         :name "Income"}])))
 
 (deftest budget-list
   (let [context (serialization/realize storage-spec budgets-context)
