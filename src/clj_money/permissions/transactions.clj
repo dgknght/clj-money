a5158cde6c39f7e66c839bc36276a0984abfc5a4
Doug Knight <dougbknight@gmail.com>
2017-10-26 19:16:26 -0500

    refine transaction permissions

--- src/clj_money/permissions/transactions.clj
+++ src/clj_money/permissions/transactions.clj
@@ -2,10 +2,10 @@
   (:refer-clojure :exclude [update])
   (:require [clj-money.authorization :as authorization]
             [clj-money.models.auth-helpers :refer [user-owns-entity?
-                                                   user-entity-ids
+                                                   all-user-entity-ids
                                                    user-granted-access?]]))
 
 (authorization/allow :transaction user-owns-entity?)
 (authorization/allow :transaction user-granted-access?)
 
-(authorization/set-scope :transaction {:entity-id user-entity-ids})
+(authorization/set-scope :transaction {:entity-id all-user-entity-ids})
