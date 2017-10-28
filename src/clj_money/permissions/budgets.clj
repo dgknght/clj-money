4061796067a2ccf70198a094e0e221459078db28
Doug Knight <dougbknight@gmail.com>
2017-10-28 15:08:52 -0500

    refine budget permissions

--- src/clj_money/permissions/budgets.clj
+++ src/clj_money/permissions/budgets.clj
@@ -1,16 +1,15 @@
 (ns clj-money.permissions.budgets
   (:refer-clojure :exclude [update])
   (:require [clj-money.authorization :as authorization]
-            [clj-money.models.auth-helpers :refer [user-entity-ids
+            [clj-money.models.auth-helpers :refer [all-user-entity-ids
+                                                   user-granted-access?
                                                    user-owns-entity?]]
             [clj-money.models.budgets :as budgets]))
 
 (authorization/set-scope
   :budget
-  {:entity-id user-entity-ids})
+  {:entity-id all-user-entity-ids})
 
 (authorization/allow :budget user-owns-entity?)
 
-(authorization/allow :budget-item
-                     (fn [user resource _ {storage-spec :storage-spec :as context}]
-                       (user-owns-entity? user (budgets/find-by-id storage-spec (:budget-id resource)) context)))
+(authorization/allow :budget-item user-granted-access?)
