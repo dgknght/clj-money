227c4d6faf76f8498cf4503124e57ba525531915
Doug Knight <dougbknight@gmail.com>
2017-11-04 11:24:44 -0500

    DRY up mailers

--- src/clj_money/mailers.clj
+++ src/clj_money/mailers.clj
@@ -6,22 +6,35 @@
             [selmer.parser :refer [render]]
             [clj-money.models.users :as users]))
 
+(defn- alt-body
+  [parts context]
+  (concat
+    [:alternative]
+    (map #(-> %
+              (assoc :content (-> (:template-path %)
+                                  slurp ; will this work on heroku?
+                                  (render context)))
+              (dissoc :template-path))
+         parts)))
+
+(def ^:private invite-user-parts
+  [{:type "text/plain"
+    :template-path "resources/templates/mailers/invite_user.txt"}
+   {:type "text/html"
+    :template-path "resources/templates/mailers/invite_user.html"}])
+
+(defn- invite-user-context
+  [to-user from-user]
+  {:recipient-first-name (:first-name to-user)
+   :sender-full-name (users/full-name from-user)
+   :app-name "clj-money"
+   :url "http://clj-money.com/users/abcdef"})
+
 (defn invite-user
   [to-user from-user]
-  (let [context {:recipient-first-name (:first-name to-user)
-                 :sender-full-name (users/full-name from-user)
-                 :app-name "clj-money"
-                 :url "http://clj-money.com/users/abcdef"}]
-    (send-message {:host (env :mailer-host)}
-                  {:to (:email to-user)
-                   :from (env :mailer-from)
-                   :subject "Invitation to clj-money"
-                   :body [:alternative
-                          {:type "text/plain"
-                           :content (-> "resources/templates/mailers/invite_user.txt"
-                                        slurp ; will this work on heroku?
-                                        (render context))}
-                          {:type "text/html"
-                           :content (-> "resources/templates/mailers/invite_user.html"
-                                        slurp ; will this work on heroku?
-                                        (render context))}]})))
+  (send-message {:host (env :mailer-host)}
+                {:to (:email to-user)
+                 :from (env :mailer-from)
+                 :subject "Invitation to clj-money"
+                 :body (alt-body invite-user-parts
+                                 (invite-user-context to-user from-user))}))
