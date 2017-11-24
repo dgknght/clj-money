afa3db03b1f68e4e1e88788dd787d3e7a0de61b8
Doug Knight <dougbknight@gmail.com>
2017-11-24 17:51:58 -0600

    add index on trade_date on price partitions

--- resources/db/create_partition_table_prices.sql
+++ resources/db/create_partition_table_prices.sql
@@ -3,5 +3,9 @@ create table if not exists prices{{table-suffix}} (
     trade_date >= '{{start-of-period}}' and
     trade_date < '{{start-of-next-period}}')
 ) inherits (prices_base);
+
+create unique index uk_prices{{table-suffix}}_trade_date_commodity
+  on prices{{table-suffix}} (trade_date, commodity_id);
+
 alter table prices{{table-suffix}}
   add constraint fk_prices{{table-suffix}}_commodities foreign key (commodity_id) references commodities (id) on delete cascade;
