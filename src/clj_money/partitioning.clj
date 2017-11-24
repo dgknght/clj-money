4ac07b6f2af33aab0b80716eeac12e163c912a93
Doug Knight <dougbknight@gmail.com>
2017-11-24 14:31:24 -0600

    allow for yearly or monthly partitions

--- src/clj_money/partitioning.clj
+++ src/clj_money/partitioning.clj
@@ -2,21 +2,35 @@
 (ns clj-money.partitioning
   (:require [clojure.java.jdbc :as jdbc]
             [clojure.pprint :refer [pprint]]
+            [clojure.tools.logging :as log]
             [clj-time.core :as t]
             [clj-time.coerce :refer [to-local-date]]
             [clj-time.periodic :refer [periodic-seq]]
             [environ.core :refer [env]]
             [selmer.parser :refer [render]]
-            [clj-money.util :refer [to-sql-date]]))
+            [clj-money.util :refer [to-sql-date
+                                    pprint-and-return]]))
 
-(defn- table-suffix
-  "Given a date, returns the suffix for the table where the
-  date belongs"
+(defn partition-period
+  [& _]
+  (keyword (env :partition-period)))
+
+(defmulti table-suffix* partition-period)
+
+(defmethod table-suffix* :year
+  [date]
+  (format "_%s" (t/year date)))
+
+(defmethod table-suffix* :month
+  [date]
+  (let [[year month] ((juxt t/year t/month) (to-local-date date))]
+    (format "_%04d_%02d" year month)))
+
+(defn table-suffix
   [date]
   (if-not date
     (throw (IllegalArgumentException. "Argument \"date\" cannot be nil")))
-  (let [[year month] ((juxt t/year t/month) (to-local-date date))]
-    (format "_%04d_%02d" year month)))
+  (table-suffix* (to-local-date date)))
 
 (defn table-name
   "Given a date and a base table name, returns the name
@@ -24,21 +38,46 @@
   [date root]
   (format "%s%s" (name root) (table-suffix date)))
 
-; This part if specific to the strategy for
-; segementing the data. E.g., monthly, quarterly, anually, etc.
-; This implementionis monthly
-(defn- partition-dates
-  "Given any two dates, calculates marker dates
-  for each partition period required to store dates
-  in the range"
-  [start-date end-date]
-  (periodic-seq (t/first-day-of-the-month start-date)
-                (t/last-day-of-the-month end-date)
-                (t/months 1)))
+(defn- descending-periodic-seq
+  ([end period-like]
+   (let [period (.toPeriod period-like)]
+     (map (fn [i]
+
+            (log/debug "dbk descending-periodic-seq " end " index " i)
+
+            (t/minus end (.multipliedBy period i)))
+          (iterate inc 0))))
+  ([start end period-like]
+   (let [period (.toPeriod period-like)]
+     (take-while (fn [next]
+                   (t/after? next start))
+                 (descending-periodic-seq end period-like)))))
+
+(defmulti ^:private partition-dates partition-period)
+
+(defmethod partition-dates :month
+  [start-date end-date options]
+  (let [seq-fn (if (:descending? options)
+                 descending-periodic-seq
+                 periodic-seq)]
+    (seq-fn (t/first-day-of-the-month start-date)
+            (t/last-day-of-the-month end-date)
+            (t/months 1))))
+
+(defmethod partition-dates :year
+  [start-date end-date options]
+  (let [seq-fn (if (:descending? options)
+                 descending-periodic-seq
+                 periodic-seq)]
+    (seq-fn (t/local-date (t/year start-date) 1 1)
+            (t/local-date (t/year end-date) 12 31)
+            (t/years 1))))
 
 (defn tables-for-range
-  [start end root]
-  (->> (partition-dates (to-local-date start) (to-local-date end))
+  [start end root options]
+  (->> (partition-dates (to-local-date start)
+                        (to-local-date end)
+                        options)
        (map #(table-name % (name root)))))
 
 (def ^:private tables
@@ -49,31 +88,34 @@
 (def ^:private create-table-format
   "create table %s (check (transaction_date >= '%s' and transaction_date < '%s')) inherits (%s_base);")
 
-(defn- start-of-period
-  "Given a date, returns the date that marks the start of the
-  period to which the date belongs"
-  [date]
-  (t/first-day-of-the-month date))
+(defmulti ^:private period-range partition-period)
 
-(defn- start-of-next-period
-  "Given a date, returns the date that marks the start of
-  the period that follows the period that contains the date"
+(defmethod period-range :year
   [date]
-  (t/plus (start-of-period date) (t/months 1)))
+  (let [start-of-period (t/local-date (t/year date) 1 1)]
+    [start-of-period
+     (t/plus start-of-period (t/years 1))]))
+
+(defmethod period-range :month
+  [date]
+  (let [start-of-period (t/first-day-of-the-month date)]
+    [start-of-period
+     (t/plus start-of-period (t/months 1))]))
 
 (defn- create-table-cmd
   [date table]
+  (let [[start next-start] (period-range date)]
   (render (slurp (format "resources/db/create_partition_table_%s.sql" table))
           {:table-suffix (table-suffix date)
-           :start-of-period (to-local-date (start-of-period date))
-           :start-of-next-period (to-local-date (start-of-next-period date))}))
+           :start-of-period start
+           :start-of-next-period next-start})))
 
 (defn- create-table-cmds
   "Given any two dates, calculates the tables that need to
   be created to accomodate data within the range and returns
   the commands to create them"
   [start-date end-date]
-  (let [dates (partition-dates start-date end-date)]
+  (let [dates (partition-dates start-date end-date {})]
     (->> tables
          (map name)
          (mapcat (fn [table]
