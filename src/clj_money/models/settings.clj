47a3260052fc0c6625716758d46cf515c65d02cf
Doug Knight <dougbknight@gmail.com>
2017-11-25 17:02:45 -0600

    remove dead code

--- src/clj_money/models/settings.clj
+++ src/clj_money/models/settings.clj
@@ -1,30 +1,14 @@
 (ns clj-money.models.settings
   (:refer-clojure :exclude [get])
   (:require [clojure.pprint :refer [pprint]]
-            [clojure.edn :as edn]
-            [clj-time.core :refer [local-date]]
-            [clj-time.format :refer [parse-local-date
-                                     unparse-local-date
-                                     formatters]]
             [clj-money.models.helpers :refer [with-storage]]
             [clj-money.models.storage :refer [put-setting
-                                              get-setting]])
-  (:import org.joda.time.LocalDate))
-
-(defmethod print-method LocalDate [this ^java.io.Writer w]
-  (doto w
-    (.write "#local-date \"")
-    (.write (unparse-local-date (:year-month-day formatters) this))
-    (.write "\"")))
+                                              get-setting]]))
 
 (defn put
   [storage-spec setting-name setting-value]
   (with-storage [s storage-spec]
     (put-setting s (name setting-name) (prn-str setting-value))))
-
-(defn- parse
-  [value]
-  (parse-local-date (:year-month-day formatters) value))
 
 (defn get
   [storage-spec setting-name]
