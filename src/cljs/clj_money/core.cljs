fd503bbff168769ddc4d5152b39d2904ba3ce3da
Doug Knight <dknight@getaroom.com>
2018-04-30 13:33:22 -0500

    working on reagent baby steps

--- src/cljs/clj_money/core.cljs
+++ src/cljs/clj_money/core.cljs
@@ -1,7 +1,13 @@
 (ns clj-money.core
-  (:require [reagent.core :as reagent]
+  (:require [reagent.core :as r]
             [secretary.core :as secretary :include-macros true]
             [accountant.core :as accountant]))
+
+(def app-state (r/atom {}))
+
+(def entities
+  [{:id 1 :name "Personal"}
+   {:id 2 :name "Business"}])
 
 (defn nav []
   [:nav.navbar.navbar-inverse
@@ -21,11 +27,29 @@
 
     [:div#navbar.collapse.navbar-collapse
      [:ul.nav.navbar-nav
-      [:li {:class "active"}
-       [:a {:href "#"} "About"]]
-      [:li
-       [:a {:href "#contact"}
-        "Contact"]]]]]])
+      [:li.dropdown {:role "presentation"}
+       [:a.dropdown-toggle
+        {:href "#"
+         :data-toggle "dropdown"
+         :role "button"
+         :aria-haspopup true}
+        (-> @app-state :current-entity :name)
+        [:span.caret]]
+       [:ul.dropdown-menu
+        (concat
+          (for [entity entities]
+            ^{:key entity} [:li {:class (when (= (:id entity)
+                                                 (:id (:current-entity @app-state)))
+                                          "active")}
+                            [:a {:href "#"
+                                 :on-click (fn []
+                                             (.log js/console "selected entity")
+                                             (swap! app-state
+                                                    #(assoc % :currency-entity entity)))}
+                             (:name entity)]])
+          [[:li.divider {:role "separator"}]
+           [:li
+            [:a {:href "#"} "Manage Entities"]]])]]]]]])
 
 (defn home-page []
   [:div
@@ -37,7 +61,7 @@
   (.log js/console "exercise the root route"))
 
 (defn mount-root []
-  (reagent/render home-page (.getElementById js/document "app")))
+  (r/render home-page (.getElementById js/document "app")))
 
 (defn init! []
   (accountant/configure-navigation!
@@ -45,5 +69,7 @@
      :path-exists? #(secretary/locate-route %)})
   (accountant/dispatch-current!)
   (mount-root))
+
+(swap! app-state #(assoc % :current-entity (first entities)))
 
 (init!)
