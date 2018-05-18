c7886468d76d06a522c9cfdb0f5e241183150558
Doug Knight <dknight@getaroom.com>
2018-05-18 14:57:22 -0500

    add utility notification fns

--- src/cljs/clj_money/notifications.cljs
+++ src/cljs/clj_money/notifications.cljs
@@ -9,6 +9,12 @@
    (swap! notifications #(conj % {:message message
                                   :severity severity}))))
 
+(defn success [message] (notify message :success))
+(defn info    [message] (notify message :info))
+(defn warning [message] (notify message :warning))
+(defn warn    [message] (notify message :warning))
+(defn danger  [message] (notify message :danger))
+
 (defn- remove-at
   [coll index]
   (vec (concat (subvec coll 0 index)
