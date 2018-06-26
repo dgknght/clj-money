3889d2dc2835f7c76baaccf2edcfdc5ba179a028
Doug Knight <dknight@getaroom.com>
2018-06-26 16:56:22 -0500

    advance commodities client side implementation

--- src/cljs/clj_money/commodities.cljs
+++ src/cljs/clj_money/commodities.cljs
@@ -10,13 +10,6 @@
             [clj-money.forms :refer [text-input
                                      required]]))
 
-; TODO need to empty this on change of entity, and probably also when navigating away
-(defonce commodities (r/atom []))
-
-(defn- load-commodities
-  []
-  (data/get-commodities (:id @state/current-entity) #(reset! commodities %)))
-
 (defn- commodity-row
   [commodity]
   ^{:key (:id commodity)}
@@ -27,17 +20,19 @@
    [:td "coming soon..."]])
 
 (defn- commodity-list []
-  #_(load-commodities)
-  (fn []
-    [:table.table.table-stripped.table-hover
-     [:tbody
-      [:tr
-       [:th "Name"]
-       [:th "Symbol"]
-       [:th "Exchange"]
-       [:th "Latest Price"]]
-      (for [commodity @commodities]
-        (commodity-row commodity))]]))
+  (let [commodities (r/atom [])]
+    (data/get-commodities (:id @state/current-entity)
+                          #(reset! commodities %))
+    (fn []
+      [:table.table.table-striped.table-hover
+       [:tbody
+        [:tr
+         [:th "Name"]
+         [:th "Symbol"]
+         [:th "Exchange"]
+         [:th "Latest Price"]]
+        (for [commodity @commodities]
+          (commodity-row commodity))]])))
 
 (defn commodities-page []
   (with-layout
@@ -46,4 +41,4 @@
        [commodity-list]]))
 
 (secretary/defroute "/commodities" []
-  (r/render commodities-page (app-element)))
+  (r/render [commodities-page] (app-element)))
