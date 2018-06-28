8eedb082d743e0e6e917e034a0a62bc041e19c6b
Doug Knight <dougbknight@gmail.com>
2018-06-27 21:44:03 -0500

    continue fleshing out commodities page

--- src/cljs/clj_money/commodities.cljs
+++ src/cljs/clj_money/commodities.cljs
@@ -7,7 +7,9 @@
             [clj-money.notifications :as notify]
             [clj-money.dom :refer [app-element]]
             [clj-money.layout :refer [with-layout]]
+            [clj-money.util :as util]
             [clj-money.forms :refer [text-input
+                                     select-input
                                      required]]))
 
 (defn- commodity-row
@@ -34,11 +36,39 @@
         (for [commodity @commodities]
           (commodity-row commodity))]])))
 
-(defn commodities-page []
+(defn- commodities-page []
   (with-layout
     [:section
-       [:h1 "Commodities"]
-       [commodity-list]]))
+     [:h1 "Commodities"]
+     [commodity-list]
+     (util/add-button "/commodities/new")]))
+
+(defn- create-commodity
+  [commodity]
+  (.log js/console "create commodity " (prn-str commodity)))
+
+(def ^:private commodity-form
+  [:form
+   (select-input :type ["currency" "stock"])
+   (select-input :exchange ["nyse" "nasdaq"] {:visible? #(= (:type %) "stock")})
+   (text-input :symbol :required)
+   (text-input :name :required)])
+
+(defn- new-commodity []
+  (let [commodity (r/atom {})]
+  (with-layout
+    [:div.row
+     [:div.col-md-6
+     [:h1 "New Commodity"]
+     [bind-fields commodity-form commodity]
+     (util/button "Save" #(create-commodity @commodity) {:class "btn btn-primary"
+                                                         :icon :ok})
+     (util/space)
+     (util/link-to "Cancel" "/commodities" {:class "btn btn-danger"
+                                            :icon :ban-circle})]])))
+
+(secretary/defroute "/commodities/new" []
+  (r/render [new-commodity] (app-element)))
 
 (secretary/defroute "/commodities" []
   (r/render [commodities-page] (app-element)))
