0511b95eea55df72235671c66e07573abc3a016c
Doug Knight <dougbknight@gmail.com>
2018-07-22 13:17:47 -0500

    continue to work on account form

--- src/cljs/clj_money/views/accounts.cljs
+++ src/cljs/clj_money/views/accounts.cljs
@@ -13,14 +13,14 @@
                                      select-input
                                      required]]))
 
-(def ^:private accounts (r/atom []))
+(def ^:private *accounts* (r/atom []))
 
 (defn- delete
   [account accounts]
   (js/alert "not implemented."))
 
 (defn- account-row
-  [account depth accounts]
+  [account depth]
   ^{:key (str "account-" (:id account))}
   [:tr
    [:td [:span {:class (str "account-depth-" depth)}
@@ -33,56 +33,80 @@
                     :class "btn btn-info btn-xs"
                     :title "Click here to edit this account."})
      (util/button nil
-                  #(delete account accounts)
+                  #(delete account *accounts*)
                   {:icon :remove
                    :class "btn btn-danger btn-xs"
                    :title "Click here to remove this account."})]]])
 
 (defn- account-and-child-rows
-  ([account accounts]
-   (account-and-child-rows account accounts 0 []))
-  ([account accounts depth result]
+  ([account]
+   (account-and-child-rows account 0 []))
+  ([account depth result]
    (-> []
-       (conj (account-row account depth accounts))
-       (concat (map #(account-and-child-rows % accounts (+ 1 depth) result)
+       (conj (account-row account depth))
+       (concat (map #(account-and-child-rows % (+ 1 depth) result)
                     (:children account))))))
 
 (defn- account-type-rows
-  [{:keys [type accounts] :as group} all-accounts]
+  [{:keys [type accounts] :as group}]
   (-> '()
       (conj ^{:key (str "account-type" type)}
             [:tr.account-type {:id (str "account-type-" type)}
              [:td type]
              [:td (util/space)]])
-      (concat (mapcat #(account-and-child-rows % all-accounts) accounts))))
+      (concat (mapcat #(account-and-child-rows %) accounts))))
 
 (defn- account-list
-  [accounts]
+  []
   [:table.table.table-striped.table-hover
    [:tbody
     [:tr
      [:th "Name"]
      [:th (util/space)]]
-    (for [row (mapcat #(account-type-rows % accounts) (nest @accounts))]
+    (for [row (mapcat #(account-type-rows %) (nest @*accounts*))]
       row)]])
 
 (defn- accounts-page []
+  (accounts/get-all (:id @state/current-entity)
+                    #(reset! *accounts* %)
+                    notify/danger)
   (with-layout
     [:section
      [:h1 "Accounts"]
-     (accounts/get-all (:id @state/current-entity)
-                       #(reset! accounts %)
-                       notify/danger)
-     [account-list accounts]
+     [account-list]
      (util/add-button "/accounts/new")]))
+
+(defn- accounts-source
+  [text]
+  (let [result (->> @*accounts*
+                    (filter #(not= -1 (-> (:name %)
+                                          (.toLowerCase)
+                                          (.indexOf text))))
+                    (mapv (juxt :name :id)))]
+    result))
 
 (def ^:private account-form
   [:form
    (text-input :name :required)
-   (select-input :parent-id (map ))
-   ])
+   [:div.form-group
+    [:label.control-label {:for :parent-id} "Parent"]
+    [:div {:field :typeahead
+           :id :parent-id
+           :input-class "form-control"
+           :list-class "typeahead-list"
+           :item-class "typeahead-item"
+           :data-source accounts-source
+           :in-fn (fn [id]
+                    ((juxt :name :id) (->> @*accounts*
+                                           (filter #(= id (:id %)))
+                                           first)))
+           :out-fn (fn [[name id]] id)
+           :result-fn (fn [[name id]] name)}]]])
 
 (defn- new-account []
+  (accounts/get-all (:id @state/current-entity)
+                    #(reset! *accounts* %)
+                    notify/danger)
   (with-layout
     [:div.row
      [:div.col-md-6
