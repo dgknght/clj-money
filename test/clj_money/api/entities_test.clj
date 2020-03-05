2a062ce618bd9cca630bb2221272b060ae8a5117
Doug Knight <dougbknight@gmail.com>
2020-03-05 10:56:32 -0600

    remove dead code

--- test/clj_money/api/entities_test.clj
+++ test/clj_money/api/entities_test.clj
@@ -136,36 +136,3 @@
 
 (deftest a-user-cannot-delete-anothers-entity
   (assert-blocked-delete (delete-an-entity "jane@doe.com")))
-
-;(defn- find-user        [ctx] (h/find-user ctx "john@doe.com"))
-;(defn- find-other-user  [ctx] (h/find-user ctx "jane@doe.com"))
-;(defn- find-entity      [ctx] (h/find-entity ctx "Personal"))
-;(defn- find-resource    [ctx] (h/find-entity ctx "Personal"))
-;(defn- select-resources [ctx] (entities/select storage-spec (:id (find-user ctx))))
-;
-;(def ^:private context
-;  {:users [(factory :user {:email "john@doe.com"})
-;           (factory :user {:email "jane@doe.com"})]
-;   :entities [{:user-id "john@doe.com"
-;               :name "Personal"}]})
-;
-;(deftest-create create-an-entity
-;  {:resource-name "entity"
-;   :create-fn api/create
-;   :create-params-fn (fn [_]
-;                       {:name "Business"
-;                        :settings {:inventory-method "fifo"}})
-;   :compare-fn #(= (:name %) "Business")
-;   :skip-auth-failure-test true})
-;
-;(deftest-update update-an-entity
-;  {:resource-name "entity"
-;   :find-updated-resource-fn #(entities/find-by-id storage-spec (:id %))
-;   :update-fn api/update
-;   :comparison-fn #(= (:name %) "My Stuff")
-;   :update-params {:name "My Stuff"
-;                   :settings {:inventory-method "fifo"}}})
-;
-;(deftest-delete delete-an-entity
-;  {:resource-name "entity"
-;   :delete-fn api/delete })
