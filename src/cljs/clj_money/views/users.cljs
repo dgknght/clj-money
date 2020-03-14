b817e6aa72db333930a2f7e5feb0b904839a98c1
Doug Knight <dougbknight@gmail.com>
2020-03-14 17:14:07 -0500

    add google logo

--- src/cljs/clj_money/views/users.cljs
+++ src/cljs/clj_money/views/users.cljs
@@ -1,13 +1,15 @@
 (ns clj-money.views.users
   (:require [secretary.core :as secretary :include-macros true]
-            [clj-money.state :refer [app-state]]))
+            [clj-money.state :refer [app-state]]
+            [clj-money.util :refer [google-g]]))
 
 (defn- login []
   [:div
    [:h1 "Login"]
-   [:a.btn.btn-primary {:href "/auth/google/start"
-                        :title "Click here to sign in with a Google account"}
-    "Google"]])
+   [:a#login.btn.btn-default {:href "/auth/google/start"
+                              :title "Click here to sign in with a Google account"}
+    (google-g)
+    [:span "Sign in with Google"]]])
 
 (secretary/defroute "/login" []
   (swap! app-state assoc :page #'login))
