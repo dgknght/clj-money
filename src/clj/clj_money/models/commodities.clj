dbbcec72a57c7fb43ce77f25122785e1c5145e74
Doug Knight <dougbknight@gmail.com>
2020-05-08 11:58:15 -0500

    remove invalid pre condition

--- src/clj/clj_money/models/commodities.clj
+++ src/clj/clj_money/models/commodities.clj
@@ -58,19 +58,11 @@
    (coercion/rule :integer [:id])
    (coercion/rule :keyword [:type])])
 
-(defn present?
-  [value]
-  (if (coll? value)
-    (seq value)
-    value))
-
 (defn search
   "Returns commodities matching the specified criteria"
   ([storage-spec criteria]
    (search storage-spec criteria {}))
   ([storage-spec criteria options]
-   {:pre [(or (-> criteria :id present?)
-              (-> criteria :entity-id present?))]}
    (with-storage [s storage-spec]
      (map after-read
           (storage/select s
