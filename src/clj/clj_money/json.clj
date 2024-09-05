1fd605600c332925e463b45eb3676a92196c0c6d
Doug Knight <dougbknight@gmail.com>
2024-09-05 17:43:20 -0500

    add encoder for java.time.LocalDate

--- src/clj/clj_money/json.clj
+++ src/clj/clj_money/json.clj
@@ -1,8 +1,14 @@
 (ns clj-money.json
   (:refer-clojure :exclude [update])
   (:require [cheshire.generate :refer [add-encoder]]
+            [java-time.api :as t]
             [dgknght.app-lib.web :refer [serialize-date
                                          serialize-date-time]]))
+
+(add-encoder
+  java.time.LocalDate
+  (fn [date gen]
+    (.writeString gen (t/format (t/formatter :iso-date) date))))
 
 (add-encoder
   org.joda.time.DateTime
