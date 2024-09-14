80f03248792b36a0f1e83e5de2c176c1a4c46d36
Doug Knight <dougbknight@gmail.com>
2024-09-14 12:04:53 -0500

    restore clj-http.core refs

--- test/clj_money/prices/yahoo_test.clj
+++ test/clj_money/prices/yahoo_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer [deftest is] :as test]
             [clojure.java.io :as io]
             [clj-time.core :as t]
+            [clj-http.core :as http]
             [dgknght.app-lib.test]
             [dgknght.app-lib.test-assertions]
             [dgknght.app-lib.web-mocks :refer [with-web-mocks]]
