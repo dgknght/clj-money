3568e4564e49597478315e428858f43cc5e4130f
Doug Knight <dougbknight@gmail.com>
2024-10-01 18:48:01 -0500

    move some sql fns from multi-money

--- src/clj_money/db/sql.clj
+++ src/clj_money/db/sql.clj
@@ -1,10 +1,64 @@
 (ns clj-money.db.sql
   (:require [next.jdbc :as jdbc]
+            [next.jdbc.plan :refer [select!
+                                    select-one!]]
+            [next.jdbc.sql.builder :refer [for-insert
+                                           for-update
+                                           for-delete]]
+            [dgknght.app-lib.inflection :refer [plural]]
             [clj-money.db :as db]
             [clj-money.db.sql.types :refer [temp-id?]]))
 
+(def ^:private infer-table-name
+  (comp keyword
+        plural
+        utl/qualifier))
+
+(defn- insert
+  [db model]
+  (let [table (infer-table-name model)
+        s (for-insert table
+                      model
+                      jdbc/snake-kebab-opts)
+        result (jdbc/execute-one! db s {:return-keys [:id]})]
+
+    ; TODO: scrub for sensitive data
+    (log/debugf "database insert %s -> %s" model s)
+    (get-in result [(keyword (name table) "id")])))
+
+(defn- update
+  [db model]
+  {:pre [(:id model)]}
+  (let [table (infer-table-name model)
+        s (for-update table
+                      (dissoc model :id)
+                      {:id (id model)}
+                      jdbc/snake-kebab-opts)
+        result (jdbc/execute-one! db s {:return-keys [:id]})]
+
+    ; TODO: scrub sensitive data
+    (log/debugf "database update %s -> %s" model s)
+
+    (get-in result [(keyword (name table) "id")])))
+
+(defn delete-one
+  [db m]
+  (let [s (for-delete (infer-table-name m)
+                      {:id (:id m)} ; TODO: find the id attribute
+                      {})]
+
+    ; TODO: scrub sensitive data
+    (log/debugf "database delete %s -> %s" m s)
+
+    (jdbc/execute! db s)
+    1))
+
 (defn- put-one
-  [_ds [_action _model]])
+  [db [oper model]]
+  (case oper
+    ::db/insert (insert db model)
+    ::db/update (update db model)
+    ::db/delete (delete-one db model)))
 
 (defmulti before-save db/type-dispatch)
 
