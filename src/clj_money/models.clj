65a83bf0d583b886448f23ad6a5507293cc62e44
Doug Knight <dougbknight@gmail.com>
2024-10-12 10:50:51 -0500

    fix count implementation

--- src/clj_money/models.clj
+++ src/clj_money/models.clj
@@ -30,7 +30,7 @@
 
 (defn count
   [criteria]
-  (:record-count (select criteria {:count true})))
+  (:record-count (db/select (db/storage) criteria {:count true})))
 
 (defn find-by
   ([criteria] (find-by criteria {}))
