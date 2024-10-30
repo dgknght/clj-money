4e56ecbdc8a49571729b0b116d9065d14a5c419e
Doug Knight <dougbknight@gmail.com>
2024-10-29 19:04:31 -0500

    don't try to replace a non-temp id

--- src/clj_money/db/sql/transaction_items.clj
+++ src/clj_money/db/sql/transaction_items.clj
@@ -1,7 +1,8 @@
 (ns clj-money.db.sql.transaction-items
   (:require [clojure.pprint :refer [pprint]]
             [stowaway.criteria :as criteria]
-            [clj-money.db.sql :as sql]))
+            [clj-money.db.sql :as sql]
+            [clj-money.db.sql.types :refer [temp-id?]]))
 
 (declare ->sql-refs)
 (sql/def->sql-refs ->sql-refs :transaction-item/account :transaction-item/transaction)
@@ -10,8 +11,10 @@
 (sql/def->model-refs ->model-refs :transaction-item/account :transaction-item/transaction)
 
 (defmethod sql/resolve-temp-ids :transaction-item
-  [item id-map]
-  (update-in item [:transaction-item/transaction-id] id-map))
+  [{:transaction-item/keys [transaction-id] :as item} id-map]
+  (if (temp-id? transaction-id)
+    (update-in item [:transaction-item/transaction-id] id-map)
+    item))
 
 (defmethod sql/prepare-criteria :transaction-item
   [criteria]
