0d8e1699304ea6a9e1c39684196affcd59ffcffe
Doug Knight <dougbknight@gmail.com>
2024-11-17 11:44:10 -0600

    add reconciliations to models/ref

--- src/clj_money/models/ref.clj
+++ src/clj_money/models/ref.clj
@@ -1,7 +1,8 @@
 (ns clj-money.models.ref
-  (:require [clj-money.models.users]
-            [clj-money.models.entities]
-            [clj-money.models.commodities]
-            [clj-money.models.accounts]
-            [clj-money.models.transactions]))
+  (:require clj-money.models.users
+            clj-money.models.entities
+            clj-money.models.commodities
+            clj-money.models.accounts
+            clj-money.models.transactions
+            clj-money.models.reconciliations))
 
