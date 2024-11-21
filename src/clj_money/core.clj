44c33023de744026a67eb046c758a0b4e67fc8f1
Doug Knight <dgknght@users.noreply.github.com>
2024-11-21 13:22:15 -0600

    remove print method for unused type (#11)

--- src/clj_money/core.clj
+++ src/clj_money/core.clj
@@ -1,12 +1,5 @@
 (ns clj-money.core
-  (:require [java-time.api :as t]
-            [dgknght.app-lib.web :refer [serialize-date]]))
-
-(defmethod print-method org.joda.time.LocalDate [this ^java.io.Writer w]
-  (doto w
-    (.write "#joda-local-date \"")
-    (.write (serialize-date this))
-    (.write "\"")))
+  (:require [java-time.api :as t]))
 
 (defmethod print-method java.time.LocalDate [this ^java.io.Writer w]
   (doto w
