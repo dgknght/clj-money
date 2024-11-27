a798f6f0415b4615ff342f37b793900f227ce461
Doug Knight <dougbknight@gmail.com>
2024-11-27 15:43:17 -0600

    add relationship between accounts and trx items

--- src/clj_money/db/sql/queries.clj
+++ src/clj_money/db/sql/queries.clj
@@ -10,6 +10,7 @@
                     [:entities :commodities]
                     [:entities :accounts]
                     [:entities :transactions]
+                    [:accounts :transaction_items]
                     [:transactions :transaction_items]
                     [:transactions :attachments]}})
 
