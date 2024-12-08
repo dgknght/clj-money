b339ca78146129070ae9940da9878e38c8670638
Doug Knight <dougbknight@gmail.com>
2024-12-08 13:20:16 -0600

    delete dead code in models/lots

--- src/clj_money/models/lots.clj
+++ src/clj_money/models/lots.clj
@@ -2,14 +2,10 @@
   (:refer-clojure :exclude [update find])
   (:require [clojure.spec.alpha :as s]
             [clojure.tools.logging :as log]
-            [config.core :refer [env]]
             [java-time.api :as t]
-            [stowaway.core :refer [tag]]
-            [stowaway.implicit :as storage :refer [with-storage]]
-            [dgknght.app-lib.models :refer [->id]]
-            [dgknght.app-lib.validation :as v :refer [with-validation]]
+            [dgknght.app-lib.validation :as v]
+            [clj-money.db :as db]
             [clj-money.models :as models]
-            [clj-money.models.commodities :as commodities]
             [clj-money.models.prices :as prices]))
 
 (defn- asset-account?
@@ -32,93 +28,37 @@
                                   :lot/shares-purchased]
                             :opt [:lot/shares-owned]))
 
-(defn- after-read
-  [lot]
-  (-> lot
-      (tag ::models/lot)
-      (update-in [:purchase-date] t/local-date)))
-
-(defn search
-  ([criteria]
-   (search criteria {}))
-  ([criteria options]
-   (with-storage (env :db)
-     (map after-read
-          (storage/select (tag criteria ::models/lot)
-                          options)))))
-
-(defn find-by
-  ([criteria]
-   (find-by criteria {}))
-  ([criteria options]
-   (first (search criteria (merge options {:limit 1})))))
-
-(defn find
-  [lot-or-id]
-  (find-by {:id (->id lot-or-id)}))
-
-(defn- before-save
-  [lot]
-  (-> lot
-      (tag ::models/lot)
-      (update-in [:purchase-date] t/sql-date)
-      (update-in [:shares-owned] (fnil identity (:shares-purchased lot)))))
-
-(defn create
-  [lot]
-  (with-storage (env :db)
-    (with-validation lot ::new-lot
-      (-> lot
-          before-save
-          storage/create
-          after-read))))
-
-(defn select-by-commodity-id
-  [commodity-id]
-  (if commodity-id
-    (search {:commodity-id commodity-id})
-    []))
-
-(defn update
-  [lot]
-  (with-storage (env :db)
-    (with-validation lot ::existing-lot
-      (-> lot
-          before-save
-          storage/update)
-      (find lot))))
-
 (defn- lot-unrealized-gains
-  [{:keys [purchase-price
-           commodity-id
-           shares-owned]}
+  [{:lot/keys [purchase-price
+               commodity
+               shares-owned]}
    price-map]
   (let [cost (* purchase-price shares-owned)
-        price (or (price-map commodity-id)
+        price (or (price-map (:id commodity))
                   0M)
         value (* price shares-owned)]
     (when (= 0M price)
-      (log/errorf "Unable to find price for commodity %s to calculate unrealized gains" commodity-id))
+      (log/errorf "Unable to find price for commodity %s to calculate unrealized gains" commodity))
     (- value cost)))
 
 (defn unrealized-gains
-  [entity-id as-of]
-  (with-storage (env :db)
-    (let [lots (search {[:commodity :entity-id] entity-id
-                        :purchase-date [:<= as-of]})
-          commodity-prices (if (seq lots)
-                             (->> (commodities/search {:id (->> lots
-                                                                (map :commodity-id)
-                                                                set)})
-                                  (map (juxt :id
-                                             #(:price (prices/most-recent % as-of))))
-                                  (into {}))
-                             {})]
-      (->> lots
-           (map #(lot-unrealized-gains % commodity-prices))
-           (reduce + 0M)))))
-
-(defn delete
-  [lot]
-  (with-storage (env :db)
-    (storage/delete lot)))
+  [entity as-of]
+  (let [lots (models/select
+               (db/model-type
+                 {:commodity/entity entity
+                  :lot/purchase-date [:<= as-of]}
+                 :lot))
+        commodity-prices (if (seq lots)
+                           (->> (models/select
+                                  (db/model-type
+                                    {:id [:in (->> lots
+                                                   (map (comp :id :lot/commodity))
+                                                   set)]}
+                                    :commodity))
+                                (map (juxt :id
+                                           #(:price/price (prices/most-recent % as-of))))
+                                (into {}))
+                           {})]
+    (->> lots
+         (map #(lot-unrealized-gains % commodity-prices))
+         (reduce + 0M))))
