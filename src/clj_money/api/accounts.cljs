9796e5d0d21b65afa46cd1aac51e9543f0243f3b
Doug Knight <dougbknight@gmail.com>
2025-01-21 19:54:40 -0600

    remove after-read logic from accounts cljs api

--- src/clj_money/api/accounts.cljs
+++ src/clj_money/api/accounts.cljs
@@ -1,48 +1,9 @@
 (ns clj-money.api.accounts
   (:refer-clojure :exclude [update get])
-  (:require [dgknght.app-lib.web :refer [unserialize-date
-                                         unserialize-date-time]]
-            [dgknght.app-lib.core :refer [parse-int
-                                          update-in-if]]
-            [dgknght.app-lib.decimal :as decimal :refer [->decimal]]
+  (:require [dgknght.app-lib.core :refer [update-in-if]]
             [clj-money.util :as util]
             [clj-money.api :as api :refer [handle-ex]]
             [clj-money.state :refer [current-entity]]))
-
-(defn- set-flags ; TODO: maybe the checkbox form fn should be able to handle keywords in a set?
-  [{:keys [tags] :as account}]
-  (reduce #(assoc %1 %2 (boolean (tags %2)))
-          account
-          tags))
-
-(defn- correct-allocations
-  [allocations]
-  (->> allocations
-       (map #(-> %
-                 (update-in [0] (comp parse-int name))
-                 (update-in [1] ->decimal)))
-       (into {})))
-
-(defn- after-read
-  [account]
-  (-> account
-      (update-in [:type] keyword)
-      (update-in [:system-tags] (comp set #(map keyword %)))
-      (update-in [:user-tags] (comp set #(map keyword %)))
-      (update-in [:value] ->decimal)
-      (update-in [:quantity] ->decimal)
-      (update-in [:tags] #(->> % (map keyword) set))
-      (update-in-if [:allocations] correct-allocations)
-      set-flags
-      (update-in [:created-at] unserialize-date-time)
-      (update-in [:commodity :type] keyword)
-      (update-in [:earliest-transaction-date] unserialize-date)
-      (update-in [:latest-transaction-date] unserialize-date)))
-
-(defn- transform
-  [xf]
-  (comp (api/apply-fn after-read)
-        xf))
 
 (defn select
   ([xf]
@@ -50,14 +11,8 @@
   ([criteria xf]
    (api/get (api/path :entities (:id @current-entity) :accounts)
             criteria
-            {:transform (transform xf)
+            {:transform xf
              :handle-ex (handle-ex "Unable to get the list of acounts: %s")})))
-
-(defn get
-  [id xf]
-  (api/get (api/path :accounts id)
-           {:transform (transform xf)
-            :handle-ex "Unable to get the account: %s"}))
 
 (def ^:private attribute-keys
   [:id
@@ -80,7 +35,7 @@
                 (update-in [:account/entity] util/->model-ref)
                 (select-keys attribute-keys)
                 (util/pp-> ::ready))
-            {:transform (transform xf)
+            {:transform xf
              :handle-ex (handle-ex "Unable to create the account: %s")}))
 
 (defn update
@@ -89,7 +44,7 @@
   ([account xf ex-handler]
    (api/patch (api/path :accounts (:id account))
               (select-keys account attribute-keys)
-              {:transform (transform xf)
+              {:transform xf
                :handle-ex ex-handler})))
 
 (defn save
