a1943e838ec980f3ab45c45ba69d00c94ab645ad
Doug Knight <dougbknight@gmail.com>
2025-01-24 16:38:03 -0600

    update commodities client api to api-3

--- src/clj_money/api/commodities.cljs
+++ src/clj_money/api/commodities.cljs
@@ -1,31 +1,41 @@
 (ns clj-money.api.commodities
   (:refer-clojure :exclude [update count get])
-  (:require [clj-money.state :refer [current-entity]]
+  (:require [cljs.pprint :refer [pprint]]
+            [clj-money.util :as util]
+            [clj-money.state :refer [current-entity]]
             [clj-money.api :as api :refer [handle-ex]]))
 
 (defn count
-  [xf]
+  [criteria & {:as opts}]
   (api/get (api/path :entities (:id @current-entity) :commodities :count)
-           {:post-xf xf
-            :handle-ex (handle-ex "Unable to get a count of commodities: %s")}))
+           criteria
+           (merge
+             {:on-error (handle-ex "Unable to get a count of commodities: %s")}
+             opts)))
 
 (defn select
   [criteria & {:as opts}]
   (api/get (api/path :entities (:id @current-entity) :commodities)
            criteria
-           (assoc opts :on-error (handle-ex "Unable to retrieve the commodities: %s"))))
+           (merge
+               {:on-error (handle-ex "Unable to retrieve the commodities: %s")}
+               opts)))
 
 (defn create
   [commodity opts]
   (api/post (api/path :entities (:commodity/entity commodity) :commodities)
             commodity
-            (assoc opts :on-error (handle-ex "Unable to create the commodity: %s"))))
+            (merge
+               {:on-error (handle-ex "Unable to create the commodity: %s")}
+               opts)))
 
 (defn update
   [commodity opts]
   (api/patch (api/path :commodities commodity)
-             commodity
-             (assoc opts :on-error (handle-ex "Unable to update the commodity: %s"))))
+             (util/prune-model commodity :commodity)
+             (merge
+               {:on-error (handle-ex "Unable to update the commodity: %s")}
+               opts)))
 
 (defn save
   [commodity & {:as opts}]
@@ -35,7 +45,8 @@
     (f commodity opts)))
 
 (defn delete
-  [commodity xf]
+  [commodity & {:as opts}]
   (api/delete (api/path :commodities (:id commodity))
-              {:post-xf xf
-               :handle-ex (handle-ex "Unable to remove the commodity: %s")}))
+              (merge
+               {:on-error (handle-ex "Unable to delete the commodity: %s")}
+               opts)))
