8f92330f8c9e1003dd114ae403228f416ec98740
Doug Knight <dougbknight@gmail.com>
2025-02-21 16:56:01 -0600

    remove updates in prices/after-save

--- src/clj_money/models/prices.clj
+++ src/clj_money/models/prices.clj
@@ -12,8 +12,6 @@
             [clj-money.models.accounts :as accounts]
             [clj-money.models.settings :as settings]))
 
-(def ^:dynamic *skip-account-updates* false)
-
 (defn- trade-date-unique?
   [{:keys [id] :as price}]
   (zero?
@@ -35,57 +33,6 @@
                                            :price/price]
                                      :opt [::id])
                              trade-date-unique?))
-
-(defn- update-commodity
-  [{:price/keys [commodity trade-date] :as price}]
-  (-> commodity
-      (update-in [:commodity/earliest-price] dates/earliest trade-date)
-      (update-in [:commodity/latest-price] dates/latest trade-date)
-      (models/put))
-  price)
-
-(defn- update-entity
-  [{:price/keys [trade-date commodity] :as price}]
-  (-> (models/find (:id (:commodity/entity commodity)) :entity)
-      (update-in [:entity/settings :settings/earliest-price-date] dates/earliest trade-date)
-      (update-in [:entity/settings :settings/latest-price-date] dates/latest trade-date)
-      (models/put))
-  price)
-
-(defn- update-account
-  [account {:price/keys [trade-date price]}]
-  (-> account
-      (assoc :account/price-as-of trade-date
-             :account/value (* (get-in account [:account/quantity] 0M)
-                               price))
-      (models/put)))
-
-(defn- accounts-to-update
-  [{:price/keys [commodity trade-date]} {:keys [price-as-of]}]
-  (let [criteria (if price-as-of
-                   [:and {:account/commodity commodity
-                          :account/quantity [:> 0M]}
-                    [:or
-                     {:account/price-as-of [:< trade-date]}
-                     {:account/price-as-of nil}]]
-                   {:account/commodity commodity
-                    :account/price-as-of price-as-of})]
-    (models/select criteria)))
-
-(defn update-accounts
-  [price & opts]
-  (when-not *skip-account-updates*
-    (doseq [a (accounts-to-update price opts)]
-      (update-account a price)))
-  price)
-
-(defmethod models/after-save :price
-  [price]
-  (-> price
-      (update-in [:price/commodity] (models/find :commodity))
-      update-entity
-      update-commodity
-      update-accounts))
 
 (defn most-recent
   ([commodity]
@@ -121,23 +68,6 @@
                                                 latest-price)]}
                        {:sort [[:price/trade-date :desc]]})))))
 
-(defn rebound-commodity
-  "Given a commodity, look up the earliest and latest prices and update the
-  commodity record"
-  [commodity]
-  (let [criteria #:price{:commodity commodity
-                         :trade-date [:between
-                                      (settings/get :earliest-partition-date)
-                                      (settings/get :latest-partition-date)]}
-        earliest (models/find-by criteria {:sort [[:price/trade-date :asc]]})
-        latest (models/find-by criteria {:sort [[:price/trade-date :desc]]})]
-    (when latest
-      (update-accounts latest))
-    (when (and earliest latest)
-      (models/put (assoc commodity
-                         :commodity/earliest-price (:trade-date earliest)
-                         :commodity/latest-price (:trade-date latest))))))
-
 (defn- apply-to-account-chain
   [price]
   (fn [[{:as account :account/keys [value quantity]} & ancestors]]
@@ -151,6 +81,18 @@
   [{:price/keys [commodity price]}]
   (mapcat (apply-to-account-chain price)
           (accounts/select-with-ancestors commodity)))
+
+(defn- update-entity
+  [{:price/keys [trade-date commodity]}]
+  (-> (models/find (:commodity/entity commodity) :entity)
+      (update-in [:entity/settings :settings/earliest-price-date] dates/earliest trade-date)
+      (update-in [:entity/settings :settings/latest-price-date] dates/latest trade-date)))
+
+(defn- update-commodity
+  [{:price/keys [commodity trade-date]}]
+  (-> commodity
+      (update-in [:commodity/earliest-price] dates/earliest trade-date)
+      (update-in [:commodity/latest-price] dates/latest trade-date)))
 
 (defn- latest-price?
   [{:price/keys [commodity trade-date]}]
@@ -169,9 +111,13 @@
       price)))
 
 (defmethod models/propagate :price
-  [before after]
+  [[before after]]
   (when-let [latest (if after
                       (when (latest-price? after)
                         after)
                       (new-latest-price before))]
-    (apply-to-account-chains latest)))
+    ; TODO: contract the date boundaries on delete on update the moves the date
+    (let [price (update-in latest [:price/commodity] (models/find :commodity))]
+      (cons (update-commodity price)
+            (cons (update-entity price)
+                  (apply-to-account-chains latest))))))
