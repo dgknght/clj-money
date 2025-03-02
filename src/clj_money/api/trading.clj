e8a14c62653bb5dece2ea198e52d49407b8ebd96
Doug Knight <dougbknight@gmail.com>
2025-03-02 15:29:39 -0600

    adjust trading api

--- src/clj_money/api/trading.clj
+++ src/clj_money/api/trading.clj
@@ -1,7 +1,8 @@
 (ns clj-money.api.trading
-  (:require [dgknght.app-lib.api :as api]
+  (:require [clojure.pprint :refer [pprint]]
+            [dgknght.app-lib.api :as api]
+            [clj-money.models :as models]
             [clj-money.authorization :refer [authorize] :as authorization]
-            [clj-money.dates :as dates]
             [clj-money.authorization.trades]
             [clj-money.trading :as trading]))
 
@@ -14,8 +15,8 @@
    :trade/account])
 
 (def ^:private trade-fn-map
-  {"buy" trading/buy
-   "sell" trading/sell})
+  {:buy trading/buy
+   :sell trading/sell})
 
 (defn- trade-fn
   [{:trade/keys [action]}]
@@ -23,12 +24,9 @@
       (throw (ex-info "Invalid trade action" {:action action}))))
 
 (defn create
-  [{:keys [params body authenticated]}]
-  (let [f (trade-fn body)]
-    (-> body
-        (update-in [:trade/value] bigdec)
-        (update-in [:trade/shares] bigdec)
-        (update-in [:trade/date] dates/unserialize-local-date)
+  [{:keys [params authenticated]}]
+  (let [f (trade-fn params)]
+    (-> params
         (assoc :trade/entity {:id (:entity-id params)})
         (select-keys create-attributes)
         (authorize ::authorization/create authenticated)
