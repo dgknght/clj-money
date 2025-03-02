e51fcc0d90e65bf44c5345cd413dc471ba6edcb4
Doug Knight <dougbknight@gmail.com>
2025-03-02 16:15:40 -0600

    adjust accounts client api

--- src/clj_money/api/accounts.cljs
+++ src/clj_money/api/accounts.cljs
@@ -1,105 +1,43 @@
 (ns clj-money.api.accounts
   (:refer-clojure :exclude [update get])
-  (:require [dgknght.app-lib.web :refer [unserialize-date
-                                         unserialize-date-time]]
-            [dgknght.app-lib.core :refer [parse-int
-                                          update-in-if]]
-            [dgknght.app-lib.decimal :as decimal :refer [->decimal]]
-            [clj-money.util :as util]
-            [clj-money.api :as api :refer [handle-ex]]
+  (:require [cljs.pprint :refer [pprint]]
+            [clj-money.models :refer [prune]]
+            [clj-money.api :as api :refer [add-error-handler]]
             [clj-money.state :refer [current-entity]]))
 
-(defn- set-flags ; TODO: maybe the checkbox form fn should be able to handle keywords in a set?
-  [{:keys [tags] :as account}]
-  (reduce #(assoc %1 %2 (boolean (tags %2)))
-          account
-          tags))
-
-(defn- correct-allocations
-  [allocations]
-  (->> allocations
-       (map #(-> %
-                 (update-in [0] (comp parse-int name))
-                 (update-in [1] ->decimal)))
-       (into {})))
-
-(defn- after-read
-  [account]
-  (-> account
-      (update-in [:type] keyword)
-      (update-in [:system-tags] (comp set #(map keyword %)))
-      (update-in [:user-tags] (comp set #(map keyword %)))
-      (update-in [:value] ->decimal)
-      (update-in [:quantity] ->decimal)
-      (update-in [:tags] #(->> % (map keyword) set))
-      (update-in-if [:allocations] correct-allocations)
-      set-flags
-      (update-in [:created-at] unserialize-date-time)
-      (update-in [:commodity :type] keyword)
-      (update-in [:earliest-transaction-date] unserialize-date)
-      (update-in [:latest-transaction-date] unserialize-date)))
-
-(defn- transform
-  [xf]
-  (comp (api/apply-fn after-read)
-        xf))
-
 (defn select
-  ([xf]
-   (select {} xf))
-  ([criteria xf]
-   (api/get (api/path :entities (:id @current-entity) :accounts)
-            criteria
-            {:transform (transform xf)
-             :handle-ex (handle-ex "Unable to get the list of acounts: %s")})))
-
-(defn get
-  [id xf]
-  (api/get (api/path :accounts id)
-           {:transform (transform xf)
-            :handle-ex "Unable to get the account: %s"}))
-
-(def ^:private attribute-keys
-  [:id
-   :account/name
-   :account/entity
-   :account/type
-   :account/commodity
-   :account/parent
-   :account/allocations
-   :account/trading
-   :account/system-tags
-   :account/user-tags])
+  [criteria & {:as opts}]
+  (api/get (api/path :entities (:id @current-entity) :accounts)
+           criteria
+           (add-error-handler
+             opts
+             "Unable to get the list of accounts: %s")))
 
 (defn create
-  [account xf]
+  [account {:as opts}]
   (api/post (api/path :entities (:account/entity account) :accounts)
-            (-> account
-                (update-in-if [:account/parent] util/->model-ref)
-                (update-in [:account/commodity] util/->model-ref)
-                (update-in [:account/entity] util/->model-ref)
-                (select-keys attribute-keys)
-                (util/pp-> ::ready))
-            {:transform (transform xf)
-             :handle-ex (handle-ex "Unable to create the account: %s")}))
+            (prune account :account)
+            (add-error-handler
+              opts
+              "Unable to create the account: %s")))
 
 (defn update
-  ([account xf]
-   (update account xf (handle-ex "Unable to update the account: %s")))
-  ([account xf ex-handler]
-   (api/patch (api/path :accounts (:id account))
-              (select-keys account attribute-keys)
-              {:transform (transform xf)
-               :handle-ex ex-handler})))
+  [account {:as opts}]
+  (api/patch (api/path :accounts (:id account))
+             (prune account :account)
+             (add-error-handler
+               opts
+               "Unable to update the account: %s")))
 
 (defn save
-  [account xf]
+  [account & {:as opts}]
   (if (:id account)
-    (update account xf)
-    (create account xf)))
+    (update account opts)
+    (create account opts)))
 
 (defn delete
-  [account xf]
-  (api/delete (api/path :accounts (:id account))
-              {:transform xf
-               :handle-ex (handle-ex "Unable to delete the account: %s")}))
+  [account & {:as opts}]
+  (api/delete (api/path :accounts account)
+              (add-error-handler
+                opts
+                "Unable to delete the account: %s")))
