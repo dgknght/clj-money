058c7d26bba9fbc0fe04d1a354b06ed2edc5fa2f
Doug Knight <dougbknight@gmail.com>
2025-03-02 19:28:47 -0600

    remove prune from accounts client api

--- src/clj_money/api/accounts.cljs
+++ src/clj_money/api/accounts.cljs
@@ -1,7 +1,6 @@
 (ns clj-money.api.accounts
   (:refer-clojure :exclude [update get])
   (:require [cljs.pprint :refer [pprint]]
-            [clj-money.models :refer [prune]]
             [clj-money.api :as api :refer [add-error-handler]]
             [clj-money.state :refer [current-entity]]))
 
@@ -16,7 +15,7 @@
 (defn create
   [account {:as opts}]
   (api/post (api/path :entities (:account/entity account) :accounts)
-            (prune account :account)
+            account
             (add-error-handler
               opts
               "Unable to create the account: %s")))
@@ -24,7 +23,7 @@
 (defn update
   [account {:as opts}]
   (api/patch (api/path :accounts (:id account))
-             (prune account :account)
+             account
              (add-error-handler
                opts
                "Unable to update the account: %s")))
