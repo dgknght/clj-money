8f34373e8c1b59a55843e4a7a88a11f654941be6
Doug Knight <dougbknight@gmail.com>
2025-03-06 20:30:55 -0600

    adjust transaction items client api

--- src/clj_money/api/transaction_items.cljs
+++ src/clj_money/api/transaction_items.cljs
@@ -1,27 +1,9 @@
 (ns clj-money.api.transaction-items
   (:refer-clojure :exclude [update])
-  (:require [dgknght.app-lib.web :refer [serialize-date
-                                         unserialize-date]]
+  (:require [dgknght.app-lib.web :refer [serialize-date]]
             [lambdaisland.uri :refer [map->query-string]]
-            [dgknght.app-lib.api-async :as api]
-            [dgknght.app-lib.decimal :refer [->decimal]]
             [clj-money.state :refer [current-entity]]
-            [clj-money.api :refer [handle-ex]]))
-
-(defn after-read
-  [item]
-  (-> item
-      (update-in [:quantity] ->decimal)
-      (update-in [:value] ->decimal)
-      (update-in [:balance] ->decimal)
-      (update-in [:transaction-date] unserialize-date)
-      (update-in [:reconciliation-status] keyword)
-      (update-in [:action] keyword)))
-
-(defn- transform
-  [xf]
-  (comp (api/apply-fn after-read)
-        xf))
+            [clj-money.api :as api :refer [add-error-handler]]))
 
 (defn- prepare-criteria
   [criteria]
@@ -30,7 +12,7 @@
       (update-in [:transaction-date] #(map serialize-date %))))
 
 (defn search
-  [criteria xf]
+  [criteria & {:as opts}]
   {:pre [(:account-id criteria)
          (:transaction-date criteria)]}
 
@@ -38,11 +20,10 @@
                      (:account-id criteria)
                      :transaction-items)
            (prepare-criteria criteria)
-           {:transform (transform xf)
-            :handle-ex (handle-ex "Unable to retrieve the transaction items: %s")}))
+           (add-error-handler opts "Unable to retrieve the transaction items: %s")))
 
 (defn summarize
-  [criteria xf]
+  [criteria & {:as opts}]
   (api/get (str (api/path :entities
                           (:id @current-entity)
                           :transaction-items
@@ -53,5 +34,4 @@
                     (update-in [:transaction-date 1] serialize-date)
                     (update-in [:interval-type] name)
                     map->query-string))
-           {:transform xf
-            :handle-ex (handle-ex "Unable to retrieve the transaction item summary: %s")}))
+           (add-error-handler opts "Unable to retrieve the transaction item summary: %s")))
