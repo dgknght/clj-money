8cb86e122f479572d3667a98368a3c079fa1e7a3
Doug Knight <dougbknight@gmail.com>
2025-04-12 14:37:48 -0500

    add reconciliation-status to items

--- src/clj_money/views/transactions.cljs
+++ src/clj_money/views/transactions.cljs
@@ -256,7 +256,18 @@
 
 (defn items-table
   [page-state]
-  (let [items (r/cursor page-state [:items])
+  (let [raw-items (r/cursor page-state [:items])
+        items (make-reaction (fn []
+                               (map (fn [{:transaction-item/keys [reconciliation]
+                                          :keys [id]
+                                          :as item}]
+                                      (assoc item
+                                             :transaction-item/reconciliation-status
+                                             (cond
+                                               (nil? id) :new
+                                               reconciliation :completed
+                                               :else :unreconciled)))
+                                    @raw-items)))
         include-children? (r/cursor page-state [:include-children?])
         account (r/cursor page-state [:view-account])
         reconciliation (r/cursor page-state [:reconciliation])
