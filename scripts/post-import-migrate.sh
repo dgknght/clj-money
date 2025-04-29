f8de5d144e4e3077760b78c923987577ab1e2f24
Doug Knight <dougbknight@gmail.com>
2025-04-29 08:21:29 -0500

    remove dev password from post-import script

--- scripts/post-import-migrate.sh
+++ scripts/post-import-migrate.sh
@@ -3,5 +3,5 @@ lein migrate-account -- -u dougbknight@gmail.com -e "$1" -f "UBS Bank USA Deposi
 echo "Migrating PMP ACG..."
 lein migrate-account -- -u dougbknight@gmail.com -e "$1" -f "USA Deposit Account" -t "PMP ACG"
 echo "Removing unused commodity accounts..."
-psql -U app_user --password please01 -h localhost -c "delete from accounts where commodity_id in (select id from commodities where name = 'Vanguard Moderate Age-Based Option') and id not in (select account_id from transaction_items);" -d money_development
+psql -U app_user -h localhost -c "delete from accounts where commodity_id in (select id from commodities where name = 'Vanguard Moderate Age-Based Option') and id not in (select account_id from transaction_items);" -d money_development
 echo "Done."
