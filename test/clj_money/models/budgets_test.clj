bc180f8321945d9c9826c545ea0e837398ab95ac
Doug Knight <dougbknight@gmail.com>
2025-06-01 09:52:25 -0500

    fix budget test

--- test/clj_money/models/budgets_test.clj
+++ test/clj_money/models/budgets_test.clj
@@ -127,15 +127,18 @@
         #:budget{:entity "Personal"
                  :name "2015"
                  :start-date (t/local-date 2015 1 1)
-                 :period [3 :month]
-                 :items [#:budget-item{:account "Food"
-                                       :periods (repeat 3 100M)}
-                         #:budget-item{:account "Non-food"
-                                       :periods (repeat 3 50M)}]}))
+                 :period [3 :month]}
+        #:budget-item{:budget "2015"
+                      :account "Food"
+                      :periods (repeat 3 100M)}
+        #:budget-item{:budget "2015"
+                      :account "Non-food"
+                      :periods (repeat 3 50M)}))
 
 (deftest get-items-by-account
   (with-context get-items-context
-    (let [budget (find-budget "2015")]
+    (let [budget (models/find-by {:budget/name "2015"}
+                                 {:include #{:budget/items}})]
       (testing "a leaf account"
         (let [account (find-account "Food")
               expected [[100M 100M 100M]]
