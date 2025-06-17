0c8f99179c2ce73b275bb45fdad9966b0e03551f
Doug Knight <dougbknight@gmail.com>
2025-06-16 20:01:53 -0500

    identity model tests pass

--- test/clj_money/models/identities_test.clj
+++ test/clj_money/models/identities_test.clj
@@ -1,18 +1,17 @@
 (ns clj-money.models.identities-test
-  (:require [clojure.test :refer [deftest use-fixtures is]]
+  (:require [clojure.test :refer [is]]
             [clojure.pprint :refer [pprint]]
             [clj-factory.core :refer [factory]]
             [dgknght.app-lib.test]
             [clj-money.models.ref]
             [clj-money.db.sql.ref]
+            [clj-money.db.datomic.ref]
             [clj-money.factories.user-factory]
             [clj-money.test-context :refer [with-context
                                             find-user]]
-            [clj-money.test-helpers :refer [reset-db]]
+            [clj-money.test-helpers :refer [dbtest]]
             [clj-money.model-helpers :as helpers :refer [assert-invalid]]
             [clj-money.models.identities :as idents]))
-
-(use-fixtures :each reset-db)
 
 (def ^:private create-context
   [(factory :user {:user/email "john@doe.com"})])
@@ -26,21 +25,21 @@
   [attr]
   (helpers/assert-created attr :refs [:identity/user]))
 
-(deftest create-an-identity
+(dbtest create-an-identity
   (with-context create-context
     (assert-created (attributes))))
 
-(deftest provider-is-required
+(dbtest provider-is-required
   (with-context create-context
     (assert-invalid (dissoc (attributes) :identity/provider)
                     {:identity/provider ["Provider is required"]})))
 
-(deftest provider-id-is-required
+(dbtest provider-id-is-required
   (with-context create-context
     (assert-invalid (dissoc (attributes) :identity/provider-id)
                     {:identity/provider-id ["Provider is required"]}))) ; TODO: Allow for this to be "Provider id is required"
 
-(deftest user-is-required
+(dbtest user-is-required
   (with-context create-context
     (assert-invalid (dissoc (attributes) :identity/user)
                     {:identity/user ["User is required"]})))
@@ -58,7 +57,7 @@
    :name "John Doe"
    :family_name "Doe"})
 
-(deftest find-a-user-from-profile-via-identity
+(dbtest find-a-user-from-profile-via-identity
   (with-context find-context
     (is (comparable? #:user{:email "john@doe.com"}
                      (idents/find-or-create-from-profile
@@ -66,7 +65,7 @@
                         profile]))
         "The user record associated with the identity is returned")))
 
-(deftest find-a-user-from-profile-and-create-identity
+(dbtest find-a-user-from-profile-and-create-identity
   (with-context create-context
     (is (comparable? #:user{:email "john@doe.com"}
                      (idents/find-or-create-from-profile
@@ -74,7 +73,7 @@
                        profile]))
         "The user record having the profile email is returned")))
 
-(deftest create-a-user-from-a-profile
+(dbtest create-a-user-from-a-profile
   (is (comparable? #:user{:email "john@doe.com"
                           :first-name "John"
                           :last-name "Doe"}
