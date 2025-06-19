fb0bb647968fe113192386df75acbfcfcf72792c
Doug Knight <dougbknight@gmail.com>
2025-06-19 11:38:51 -0500

    deconstruct allocations on account save

--- src/clj_money/db/datomic/accounts.clj
+++ src/clj_money/db/datomic/accounts.clj
@@ -1,9 +1,22 @@
 (ns clj-money.db.datomic.accounts
-  (:require [dgknght.app-lib.core :refer [update-in-if]]
+  (:require [clojure.pprint :refer [pprint]]
+            [dgknght.app-lib.core :refer [update-in-if]]
             [clj-money.db.datomic :as datomic]))
+
+(defmethod datomic/deconstruct :account
+  [{:account/keys [allocations] :as account}]
+  ; TODO: Refine this to handle removing allocations that have been changed or removed
+  (cons (dissoc account :account/allocations)
+        (map (fn [[k v]]
+               [:db/add
+                (:id account)
+                :account/allocations
+                [k v]])
+             allocations)))
 
 (defmethod datomic/after-read :account
   [account]
   (-> account
       (update-in-if [:account/system-tags] set)
-      (update-in-if [:account/user-tags] set)))
+      (update-in-if [:account/user-tags] set)
+      (update-in-if [:account/allocations] #(into {} %))))
