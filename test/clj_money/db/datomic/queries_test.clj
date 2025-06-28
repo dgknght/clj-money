b9e198279d8cc0b18d654f4c9043cb63e2676b20
Doug Knight <dougbknight@gmail.com>
2025-06-27 20:54:08 -0500

    test queries for inclusion logic

--- test/clj_money/db/datomic/queries_test.clj
+++ test/clj_money/db/datomic/queries_test.clj
@@ -11,36 +11,61 @@
     [(if upward? '?e2 '?e1) rel-key (if upward? '?e1 '?e2)]
     '(match-and-recurse ?e2 ?v)]])
 
+(def accounts
+  [[1 :account/name "Checking"]
+   [2 :account/name "Savings"]
+   [3 :account/name "Car"]
+   [3 :account/parent 2]
+   [4 :account/name "Doug"]
+   [4 :account/parent 3]
+   [5 :account/name "Eli"]
+   [5 :account/parent 3]
+   [6 :account/name "Reserve"]
+   [6 :account/parent 2]])
+
 (deftest query-recursively
-  (let [accounts [[1 :account/name "Checking"]
-                  [2 :account/name "Savings"]
-                  [3 :account/name "Car"]
-                  [3 :account/parent 2]
-                  [4 :account/name "Doug"]
-                  [4 :account/parent 3]
-                  [5 :account/name "Eli"]
-                  [5 :account/parent 3]
-                  [6 :account/name "Reserve"]
-                  [6 :account/parent 2]]]
-    (testing "infinite levels downward"
-      (is (= #{"Doug" "Eli" "Car" "Reserve" "Savings"}
-             (set
-               (map first
-                    (d/q '[:find ?name
-                           :in $ % ?input
-                           :where [?a :account/name ?name]
-                           (match-and-recurse ?a ?input)]
-                         accounts
-                         (recursion-rule :account/parent :account/name false)
-                         "Savings"))))))
-    (testing "infinite levels upward"
-      (is (= #{"Doug" "Car" "Savings"}
-             (set
-               (map first
-                    (d/q '[:find ?name
-                           :in $ % ?input
-                           :where [?a :account/name ?name]
-                           (match-and-recurse ?a ?input)]
-                         accounts
-                         (recursion-rule :account/parent :account/name true)
-                         "Doug"))))))))
+  (testing "infinite levels downward"
+    (is (= #{"Doug" "Eli" "Car" "Reserve" "Savings"}
+           (set
+             (map first
+                  (d/q '[:find ?name
+                         :in $ % ?input
+                         :where [?a :account/name ?name]
+                         (match-and-recurse ?a ?input)]
+                       accounts
+                       (recursion-rule :account/parent :account/name false)
+                       "Savings"))))))
+  (testing "infinite levels upward"
+    (is (= #{"Doug" "Car" "Savings"}
+           (set
+             (map first
+                  (d/q '[:find ?name
+                         :in $ % ?input
+                         :where [?a :account/name ?name]
+                         (match-and-recurse ?a ?input)]
+                       accounts
+                       (recursion-rule :account/parent :account/name true)
+                       "Doug")))))))
+
+(deftest query-with-or
+  (testing "query by entity"
+    (is (= #{"Checking" "Reserve"}
+           (set
+             (map first
+                  (d/q '[:find ?name
+                         :in $ ?ids
+                         :where [?a :account/name ?name]
+                                [(contains? ?ids ?a)]]
+                       accounts
+                       #{1 6}))))))
+  (testing "query by attribute"
+    (is (= #{"Car" "Reserve" "Doug" "Eli"}
+           (set
+             (map first
+                  (d/q '[:find ?name
+                         :in $ ?parent
+                         :where [?a :account/parent ?account-parent]
+                                [?a :account/name ?name]
+                                [(contains? ?parent ?account-parent)]]
+                       accounts
+                       #{2 3})))))))
