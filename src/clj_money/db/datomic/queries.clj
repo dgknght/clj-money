10b356c0859a9de9724ff5b43a80382fdcf6661e
Doug Knight <dougbknight@gmail.com>
2025-06-28 15:42:43 -0500

    move relationships to shared location

--- src/clj_money/db/datomic/queries.clj
+++ src/clj_money/db/datomic/queries.clj
@@ -1,9 +1,9 @@
 (ns clj-money.db.datomic.queries
-  (:require [stowaway.datalog :as dtl]))
+  (:require [stowaway.datalog :as dtl]
+            [clj-money.models.schema :as schema]))
 
 (def ^:private default-opts
-  {:relationships #{[:user :entity]
-                    [:entity :commodity]}
+  {:relationships schema/relationships
    :query-prefix [:query]})
 
 (defn apply-criteria
