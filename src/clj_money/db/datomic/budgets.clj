3f537ff4a41bfd28f93a0c9b6da830212d34ad67
Doug Knight <dougbknight@gmail.com>
2025-07-08 15:23:38 -0500

    use apply-coercions for budgets, query items separately

--- src/clj_money/db/datomic/budgets.clj
+++
@@ -1,9 +0,0 @@
-(ns clj-money.db.datomic.budgets
-  (:require [clj-money.dates :refer [->local-date]]
-            [clj-money.db.datomic :as datomic]))
-
-(defmethod datomic/after-read :budget
-  [budget]
-  (-> budget
-      (update-in [:budget/start-date] ->local-date)
-      (update-in [:budget/end-date] ->local-date)))
