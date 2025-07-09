f6d5747d1d8e30c5072dfa27ebfa4a601a6e40ba
Doug Knight <dougbknight@gmail.com>
2025-07-08 19:11:22 -0500

    use coercions for reconciliations

--- src/clj_money/db/datomic/reconciliations.clj
+++ src/clj_money/db/datomic/reconciliations.clj
@@ -1,6 +1,5 @@
 (ns clj-money.db.datomic.reconciliations
-  (:require [clj-money.dates :refer [->local-date]]
-            [clj-money.db.datomic :as datomic]))
+  (:require [clj-money.db.datomic :as datomic]))
 
 (defmethod datomic/before-save :reconciliation
   [{:as recon :reconciliation/keys [item-refs]}]
@@ -8,8 +7,3 @@
       (dissoc :reconciliation/item-refs)
       (assoc :reconciliation/items
              (mapv first item-refs))))
-
-(defmethod datomic/after-read :reconciliation
-  [recon]
-  (update-in recon [:reconciliation/end-of-period] ->local-date))
-
