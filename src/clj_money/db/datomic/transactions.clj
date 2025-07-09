cfae63bcacf39008394238ac23620410b3292f18
Doug Knight <dougbknight@gmail.com>
2025-07-08 19:13:36 -0500

    remove after-read for transactions

--- src/clj_money/db/datomic/transactions.clj
+++ src/clj_money/db/datomic/transactions.clj
@@ -1,6 +1,5 @@
 (ns clj-money.db.datomic.transactions
   (:require [clojure.pprint :refer [pprint]]
-            [clj-money.dates :as dates]
             [clj-money.util :refer [+id temp-id]]
             [clj-money.db.datomic :as datomic]))
 
@@ -20,7 +19,3 @@
              [:transaction/items]
              (fn [items]
                (mapv #(+id % temp-id) items))))
-
-(defmethod datomic/after-read :transaction
-  [trx]
-  (update-in trx [:transaction/transaction-date] dates/->local-date))
