1ffc088859020817dd662e0e8b6cc7d61694e4e1
Doug Knight <dougbknight@gmail.com>
2025-07-08 19:09:44 -0500

    use coercions for prices

--- src/clj_money/db/datomic/ref.clj
+++ src/clj_money/db/datomic/ref.clj
@@ -3,7 +3,6 @@
             clj-money.db.datomic.commodities
             clj-money.db.datomic.accounts
             clj-money.db.datomic.transactions
-            clj-money.db.datomic.prices
             clj-money.db.datomic.grants
             clj-money.db.datomic.attachments
             clj-money.db.datomic.budget-items
