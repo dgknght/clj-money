67ae09c72ab370158753edf4157faa8f3c8eecb3
Doug Knight <dougbknight@gmail.com>
2025-07-08 19:06:28 -0500

    use coercions for lots

--- src/clj_money/db/datomic/lots.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-money.db.datomic.lots
-  (:require [clj-money.dates :refer [->local-date]]
-            [clj-money.db.datomic :as datomic]))
-
-(defmethod datomic/after-read :lot
-  [lot]
-  (update-in lot [:lot/purchase-date] ->local-date))
