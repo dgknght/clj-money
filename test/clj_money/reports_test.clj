3f61c609ddd494508431a69bded1d8f880e13373
Doug Knight <dougbknight@gmail.com>
2025-07-27 10:22:44 -0500

    adjust budget setup for tests

--- test/clj_money/reports_test.clj
+++ test/clj_money/reports_test.clj
@@ -37,7 +37,7 @@
     (is (seq-of-maps-like? fixtures/expected-balance-sheet-with-commodities
                            (reports/balance-sheet (models/find (find-entity "Personal"))
                                                   (t/local-date 2017 3 2)))
-        "THe balance sheet includes unrealized gains")))
+        "The balance sheet includes unrealized gains")))
 
 (deftest create-a-commodities-account-summary
   (with-context fixtures/commodities-account-summary-context
