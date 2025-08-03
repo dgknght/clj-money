49fea7bd8f352a7524ed1ca6ccbed08ab6be6379
Doug Knight <dougbknight@gmail.com>
2025-08-03 12:50:34 -0500

    address imports

--- src/clj_money/db/datomic/imports.clj
+++ src/clj_money/db/datomic/imports.clj
@@ -5,4 +5,12 @@
 
 (defmethod datomic/before-save :import
   [imp]
-  (update-in-if imp [:import/options] util/qualify-keys :import-option))
+  (-> imp
+      (update-in-if [:import/progress] pr-str)
+      (update-in-if [:import/options] util/qualify-keys :import-option)))
+
+(defmethod datomic/after-read :import
+  [imp]
+  (-> imp
+      (update-in-if [:import/progress] read-string)
+      (update-in-if [:import/options] update-keys (comp keyword name))))
