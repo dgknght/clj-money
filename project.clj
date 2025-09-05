76cb5d9df607050264d60ebb797de1a6f71c76bb
Doug Knight <dougbknight@gmail.com>
2025-09-05 17:06:02 -0500

    clean up conflicting dependencies

--- project.clj
+++ project.clj
@@ -149,7 +149,7 @@
                    com.fasterxml.jackson.core/jackson-core
                    lein-doo]]
                  [lambdaisland/uri "1.4.54"]
-                 [com.taoensso/carmine "3.4.1"]
+                 [com.taoensso/carmine "3.4.1" :exclusions [org.clojure/tools.reader commons-codec]]
                  [stowaway "0.2.10" :exclusions [com.github.seancorfield/honeysql org.clojure/spec.alpha org.clojure/clojure potemkin org.clojure/core.specs.alpha org.clojure/tools.logging]]]
   :repl-options {:init-ns clj-money.repl
                  :welcome (println "Welcome to better money management!")}
