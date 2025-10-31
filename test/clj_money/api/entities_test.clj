834b5fc52bc13041a59d0a508b0d5ed9bf78b16e
Doug Knight <dougbknight@gmail.com>
2025-10-31 16:55:23 -0500

    better tests for different content types

--- test/clj_money/api/entities_test.clj
+++ test/clj_money/api/entities_test.clj
@@ -1,11 +1,8 @@
 (ns clj-money.api.entities-test
   (:require [clojure.test :refer [deftest testing is use-fixtures]]
             [clojure.pprint :refer [pprint]]
-            [ring.mock.request :as req]
             [clj-factory.core :refer [factory]]
-            [muuntaja.core :as muuntaja]
             [dgknght.app-lib.web :refer [path]]
-            [dgknght.app-lib.test :refer [parse-json-body]]
             [clj-money.entities :as entities]
             [clj-money.db.ref]
             [clj-money.entities.ref]
@@ -13,10 +10,9 @@
             [clj-money.test-context :refer [with-context
                                             find-user
                                             find-entity]]
-            [clj-money.test-helpers :refer [reset-db
-                                            edn-body
-                                            parse-edn-body]]
-            [clj-money.api.test-helper :refer [add-auth]]
+            [clj-money.test-helpers :refer [reset-db]]
+            [clj-money.api.test-helper :refer [request
+                                               parse-body]]
             [clj-money.web.server :refer [app]]))
 
 (use-fixtures :each reset-db)
@@ -25,47 +21,51 @@
   [(factory :user {:user/email "john@doe.com"})
    (factory :user {:user/email "jane@doe.com"})])
 
+(defn- create-entity
+  [& {:keys [user content-type body]
+      :or {content-type "application/edn"
+           body #:entity{:name "Personal"
+                         :settings {:settings/inventory-method :fifo}}}}]
+  (-> (request :post (path :api :entities)
+               :user user
+               :content-type content-type
+               :body body)
+      app
+      parse-body))
+
 (deftest a-user-can-create-an-entity
   (with-context create-context
     (let [user (find-user "john@doe.com")]
       (testing "default format (edn)"
         (let [{:as res
-               :keys [edn-body]}
-              (-> (req/request :post (path :api :entities))
-                  (edn-body #:entity{:name "Personal"
-                                     :settings {:settings/inventory-method :fifo}})
-                  (add-auth user)
-                  app
-                  parse-edn-body)]
+               :keys [parsed-body]}
+              (create-entity :user user)]
           (is (http-success? res))
           (is (comparable? #:entity{:user (select-keys user [:id])
                                     :name "Personal"
                                     :settings {:settings/inventory-method :fifo}}
-                           (entities/find (:id edn-body)
+                           (entities/find (:id parsed-body)
                                           :entity))
               "The entity can be retrieved")))
       (testing "json format"
         (let [{:as res
-               :keys [json-body]}
-              (-> (req/request :post (path :api :entities))
-                  (req/json-body {:name "Alt-Personal"
+               :keys [parsed-body]}
+              (create-entity :user user
+                             :content-type "application/json"
+                             :body {:name "Alt-Personal"
                                   :settings {:inventory-method :fifo
                                              :_type :settings}
-                                  :_type :entity})
-                  (req/header :accept "application/json")
-                  (add-auth user)
-                  app
-                  parse-json-body)]
+                                  :_type :entity})]
           (is (http-success? res))
           (is (comparable? {:user (select-keys user [:id])
                             :name "Alt-Personal"
                             :settings {:inventoryMethod "fifo"}}
-                           json-body)
+                           parsed-body)
               "The created entity is returned")
           (is (comparable? #:entity{:user (select-keys user [:id])
                                     :name "Alt-Personal"
                                     :settings {:settings/inventory-method :fifo}}
-                           (entities/find (:id json-body)
+                           (entities/find (:id parsed-body)
                                           :entity))
               "The entity can be retrieved"))))))
 
@@ -84,25 +84,20 @@
                            #{{:id 1}
                              {:id 2}}}}
                  content-type "application/edn"}}]
-  (let [user (find-user email)
-        entity (find-entity "Personal")
-        response (-> (req/request :patch (path :api :entities (:id entity)))
-                     (req/content-type content-type)
-                     (req/header :accept content-type)
-                     (assoc :body (muuntaja/encode
-                                    content-type
-                                    changes))
-                     (add-auth user)
+  (let [entity (find-entity "Personal")
+        response (-> (request :patch (path :api :entities (:id entity))
+                              :user (when email (find-user email))
+                              :content-type content-type
+                              :body changes)
                      app
-                     parse-edn-body
-                     parse-json-body)]
+                     parse-body)]
     [response (entities/find entity)]))
 
 (defn- assert-successful-edit
   [[response retrieved]]
   (is (http-success? response))
   (is (comparable? {:entity/name "New Name"}
-                   (:edn-body response))
+                   (:parsed-body response))
       "The updated entity is returned in the response")
   (is (comparable? {:entity/name "New Name"}
                    retrieved)
@@ -131,10 +126,10 @@
         (is (http-success? res))
         (is (comparable? {:name "json name"
                           :_type "entity"}
-                         (:json-body res))
+                         (:parsed-body res))
             "The response contains the updated entity")
         (is (= #{3 4}
-               (->> (get-in res [:json-body :settings :monitoredAccounts])
+               (->> (get-in res [:parsed-body :settings :monitoredAccounts])
                     (map :id)
                     set)))
         (is (comparable? {:entity/name "json name"
@@ -150,52 +145,41 @@
 (deftest an-unauthenticated-user-cannot-edit-an-entity
   (with-context list-context
     (let [entity (find-entity "Personal")
-          response (-> (req/request :patch (path :api :entities (:id entity)))
-                       (edn-body (-> entity
-                                          (assoc :entity/name "New Name")
-                                          (select-keys [:entity/name
-                                                        :entity/settings])))
-                       app)]
+          [response] (edit-an-entity nil)]
       (is (http-unauthorized? response))
       (is (comparable? {:entity/name "Personal"}
                        (entities/find entity))
           "The retrieved value has not been changed."))))
 
 (defn- get-a-list
-  [email & {:keys [format parse]
-            :or {format "application/edn"
-                 parse parse-edn-body}}]
-  (-> (req/request :get (path :api :entities))
-      (req/content-type format)
-      (req/header "Accept" format)
-      (add-auth (find-user email))
+  [email & {:keys [format]
+            :or {format "application/edn"}}]
+  (-> (request :get (path :api :entities)
+               :content-type format
+               :user (find-user email))
       app
-      parse))
+      parse-body))
 
 (defn- assert-successful-list
-  [response & {:keys [expected response-key]}]
+  [{:as response :keys [parsed-body]} & {:keys [expected]}]
   (is (http-success? response))
   (is (seq-of-maps-like? expected
-                         (response response-key))
-      "The body contains the correct entities"))
+                         parsed-body)
+      "The body contains the entities"))
 
 (defn- assert-blocked-list
-  [{:as response :keys [edn-body]}]
+  [{:as response :keys [parsed-body]}]
   (is (http-success? response))
-  (is (empty? edn-body) "The body is empty"))
+  (is (empty? parsed-body) "The body is empty"))
 
 (deftest a-user-can-get-a-list-of-his-entities
   (with-context list-context
     (testing "default format (edn)"
       (assert-successful-list (get-a-list "john@doe.com")
-                              :response-key :edn-body
                               :expected [{:entity/name "Business"}
                                          {:entity/name "Personal"}]))
     (testing "json format"
-      (assert-successful-list (get-a-list "john@doe.com"
-                                          :format "application/json"
-                                          :parse parse-json-body)
-                              :response-key :json-body
+      (assert-successful-list (get-a-list "john@doe.com" :format "application/json")
                               :expected [{:name "Business"}
                                          {:name "Personal"}]))))
 
@@ -207,11 +191,8 @@
   [email]
   (with-context list-context
     (let [entity (find-entity "Personal")]
-      [(-> (req/request :delete
-                        (path :api
-                              :entities
-                              (:id entity)))
-           (add-auth (find-user email))
+      [(-> (request :delete (path :api :entities (:id entity))
+                    :user (find-user email))
            app)
        (entities/find entity)])))
 
