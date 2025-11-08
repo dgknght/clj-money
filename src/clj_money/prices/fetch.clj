5c884adff964cdedcd472d405a0e6b65874cac53
Doug Knight <dougbknight@gmail.com>
2025-11-08 14:55:29 -0600

    move fetch logic to its own ns

---
+++ src/clj_money/prices/fetch.clj
@@ -0,0 +1,70 @@
+(ns clj-money.prices.fetch
+  (:require [clojure.pprint :refer [pprint]]
+            [dgknght.app-lib.core :refer [index-by]]
+            [clj-money.util :refer [entity=]]
+            [clj-money.entities :as entities]
+            [clj-money.prices :as p]
+            [clj-money.prices.fetch :as f]
+            [clj-money.prices.yahoo :as yahoo]
+            [clj-money.prices.alpha-vantage :as alpha-vantage]
+            [clj-money.prices.cache :as cache]))
+
+(defn- commodity-type?
+  [types]
+  (fn [{:commodity/keys [type]}]
+    (types type)))
+
+(defn fetch
+  "Given a sequence of commodity entities, fetches prices from external services
+  and returns the price entities."
+  [commodities]
+  (let [->key (juxt :commodity/exchange
+                    :commodity/symbol)
+        mapped-commodities (index-by ->key commodities)
+        with-cache-writes (fn [p]
+                            (reify p/PriceProvider
+                              (fetch-prices [_ symbols]
+                                (let [prices (p/fetch-prices p symbols)]
+                                  (when (seq prices)
+                                    (->> prices
+                                         (map (fn [{:price/keys [value trade-date]
+                                                    :commodity/keys [symbol exchange]}]
+                                                {:cached-price/value value
+                                                 :cached-price/trade-date trade-date
+                                                 :cached-price/symbol symbol
+                                                 :cached-price/exchange exchange}))
+                                         entities/put-many
+                                         doall))
+                                  prices))))]
+    (:prices (reduce (fn [{:keys [commodities] :as m}
+                          {:keys [provider types]}]
+                       (if (empty? commodities)
+                         (reduced m)
+                         (let [prices (->> commodities
+                                           (filter (commodity-type? types))
+                                           (map :commodity/symbol)
+                                           (p/fetch-prices provider)
+                                           (mapv (fn [p]
+                                                   (let [c (mapped-commodities (->key p))]
+                                                     (-> p
+                                                         (assoc :price/commodity c)
+                                                         (dissoc :commodity/exchange
+                                                                 :commodity/symbol))))))]
+                           (-> m
+                               (update-in [:prices] concat prices)
+                               (update-in [:commodities] (fn [cs]
+                                                           (remove (fn [c]
+                                                                     (some #(entity= (:price/commodity %)
+                                                                                     c)
+                                                                           prices))
+                                                                   cs)))))))
+                     {:commodities commodities
+                      :prices []}
+                     [{:provider (cache/->CacheProvider)
+                       :types #{:fund :stock :currency}}
+                      {:provider (with-cache-writes
+                                   (yahoo/->YahooProvider))
+                       :types #{:fund :stock}}
+                      {:provider (with-cache-writes
+                                   (alpha-vantage/->AlphaVantageProvider))
+                       :types #{:currency :stock :fund}}]))))
---
+++ test/clj_money/prices/fetch_test.clj
@@ -0,0 +1,46 @@
+(ns clj-money.prices.fetch-test
+  (:require [clojure.test :refer [deftest is use-fixtures]]
+            [java-time.api :as t]
+            [dgknght.app-lib.test-assertions]
+            [dgknght.app-lib.core :refer [index-by]]
+            [clj-money.test-helpers :refer [reset-db]]
+            [clj-money.entities.ref]
+            [clj-money.entities :as e]
+            [clj-money.db.ref]
+            [clj-money.prices.fetch :as f]
+            [clj-money.prices.alpha-vantage :as alpha]
+            [clj-money.prices.yahoo :as yahoo]))
+
+(use-fixtures :each reset-db)
+
+(def ^:private alpha-data
+  (index-by :commodity/symbol [{:price/value 10M
+                                :price/trade-date (t/local-date 2016 1 1)
+                                :commodity/symbol "AAPL"
+                                :commodity/exchange :nasdaq}]))
+
+(def ^:private yahoo-data
+  (index-by :commodity-symbol [{:price/value 11M
+                                :price/trade-date (t/local-date 2016 1 1)
+                                :commodity/symbol "MSFT"
+                                :commodity/exchange :nasdaq}]))
+
+(deftest fetch-prices-from-alpha-vantage
+  (with-redefs [alpha/get-quote (fn [symbol]
+                                  (alpha-data symbol))
+                yahoo/get-quotes (fn [symbols]
+                                   (->> symbols
+                                        (map yahoo-data)
+                                        (filter identity)))]
+    (is (= [{:price/commodity #:commodity{:symbol "AAPL"
+                                          :exchange :nasdaq
+                                          :type :stock}
+             :price/value 10M
+             :price/trade-date (t/local-date 2016 1 1)}]
+           (f/fetch [#:commodity{:symbol "AAPL"
+                                 :exchange :nasdaq
+                                 :type :stock}]))
+        "The prices are returned")
+    (is (comparable? {:cached-price/value 10M}
+                     (e/find-by {:cached-price/symbol "AAPL"}))
+        "The cached price record is created")))
