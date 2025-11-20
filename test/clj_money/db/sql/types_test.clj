c724aa10385b5ad1087d4cd012948f90d8225e9b
Doug Knight <dougbknight@gmail.com>
2025-11-20 15:05:37 -0600

    use shorthand fn for QualifiedID

--- test/clj_money/db/sql/types_test.clj
+++ test/clj_money/db/sql/types_test.clj
@@ -4,29 +4,29 @@
             [clj-money.db.sql.types :as types]))
 
 (deftest serialize-an-id
-  (is (= "user:123" (str (types/->QualifiedID 123 :user)))))
+  (is (= "user:123" (str (types/qid 123 :user)))))
 
 (deftest unserialize-an-id
-  (is (types/->QualifiedID 123 :user)
+  (is (types/qid 123 :user)
       (types/unserialize-id "user:123")))
 
 (deftest qualify-an-id
-  (is (= {:id (types/->QualifiedID 123 :user)
+  (is (= {:id (types/qid 123 :user)
           :user/first-name "John"}
          (types/qualify-id {:id 123
                             :user/first-name "John"}))
       "A map is returned with the :id value updated")
-  (is (= (types/->QualifiedID 123 :user)
+  (is (= (types/qid 123 :user)
          (types/qualify-id 123 :user))
       "A id with an entity type is returned as QualifiedID")
   (is (nil? (types/qualify-id nil :user))
       "Given nil, nil is returned")
   (testing "higher order function arity"
     (let [f (types/qualify-id :user)]
-      (is (= (types/->QualifiedID 123 :user)
+      (is (= (types/qid 123 :user)
              (f 123))
           "The function returns a qualified id given an id")
-      (is (= {:id (types/->QualifiedID 123 :user)
+      (is (= {:id (types/qid 123 :user)
               :user/first-name "John"}
              (f {:id 123
                  :user/first-name "John"}))
@@ -35,20 +35,20 @@
 (deftest decompose-a-qualified-id
   (is (= {:entity-type :user
           :id 123}
-         (e/components (types/->QualifiedID 123 :user)))))
+         (e/components (types/qid 123 :user)))))
 
 (deftest test-for-equalify-between-qualified-ids
-  (is (= (types/->QualifiedID 123 :user)
-         (types/->QualifiedID 123 :user))))
+  (is (= (types/qid 123 :user)
+         (types/qid 123 :user))))
 
 (deftest convert-an-entity-for-sql-storage
   (testing "a referenced entity"
     (is (= {:id 201
             :entity/name "Personal"
             :entity/user-id 101}
-           (types/sqlize {:id (types/->QualifiedID 201 :entity)
+           (types/sqlize {:id (types/qid 201 :entity)
                           :entity/name "Personal"
-                          :entity/user {:id (types/->QualifiedID 101 :user)
+                          :entity/user {:id (types/qid 101 :user)
                                         :user/email "john@doe.com"}}
                          {:ref-keys #{:entity/user}}))))
   (testing "a nested map that is not an entity"
