f5eb2c098d8cd5dae74160176e7f0c5ce60afadd
Doug Knight <dougbknight@gmail.com>
2025-12-14 19:20:46 -0600

    add delay to tmux start script

--- scripts/start_tmux.sh
+++ scripts/start_tmux.sh
@@ -4,15 +4,19 @@ tmux new-session -d -s $session
 
 # REPL window
 tmux rename-window -t 0 'repl'
+sleep 0.5
 tmux send-keys 'clear' C-m 'lein repl' C-m
 
 tmux split-window -v
+sleep 0.5
 tmux send-keys 'lein fig:build' C-m
 
 # Code window
 tmux new-window -t $session:1 -n $session
+sleep 0.5
 tmux send-keys 'nvim' C-m
 tmux split-window -h
+sleep 0.5
 tmux send-keys 'git status' C-m
 
 # Database window
@@ -20,8 +24,10 @@ tmux new-window -t $session:2 -n 'database' 'psql'
 
 # Log window
 tmux new-window -t $session:3 -n 'logs'
+sleep 0.5
 tmux send-keys 'tail -f log/development.log | grep -e ERROR -e WARN -e dbk' C-m
 tmux split-window -v
+sleep 0.5
 tmux send-keys 'tail -f log/development.log' C-m
 
 tmux attach -t $session:1
