b0d66608348e28eb5f789dd0437cf8975bdc2914
Doug Knight <dougbknight@gmail.com>
2025-12-23 12:04:59 -0600

    add options to ptest task

--- test/clj_money/runner.clj
+++ test/clj_money/runner.clj
@@ -1,5 +1,6 @@
 (ns clj-money.runner
   (:require [clojure.pprint :refer [pprint]]
+            [clojure.tools.cli :refer [parse-opts]]
             [java-time.api :as t]
             [eftest.runner :refer [find-tests run-tests]]
             [clj-money.config :refer [env]]
@@ -26,9 +27,60 @@
     (doseq [c configs]
       (init-sql-db c))))
 
+(def ^:private eftest-options
+  [["-s" "--strategy STRATEGY" "The storage strategy"
+    :parse-fn keyword
+    :validate [#{:sql :datomic-peer} "Unrecognized storage strategy"]]
+   ["-m" "--multithread MULTITHREAD"
+    "The strategy for dividing work into threads. (Default namespaces)"
+    :id :multithread?
+    :default :namespaces
+    :parse-fn keyword
+    :validate [#{:namespaces :vars "Unrecognized multithread strategy"}]]
+   ["-v" "--verbose"
+    "Indicates whether or not to render test output for passing tests. (Default false)"
+    :parse-fn not
+    :id :capture-output?]
+   ["-h" "--help" "Show this help message"]])
+
+(defn- match-strategy?
+  [selector]
+  (if selector
+    #(= selector
+        (-> % meta :strategy))
+    (constantly true)))
+
+(defn- write-help
+  [{:keys [errors summary]}]
+  (when (seq errors)
+    (println "ERRORS:")
+    (doseq [e errors] (println "  - " e))
+    (println ""))
+  (println "lein ptest [OPTIONS] [ARGUMENTS]")
+  (println "")
+  (println "OPTIONS:")
+  (print summary)
+  (println ""))
+
+(defn- show-help?
+  [{:keys [errors arguments options]}]
+  (or (seq errors)
+            (:help options)
+            (some #(= "help" %)
+                  arguments)))
+
+(defn- eftest*
+  [{:keys [options
+           arguments]}]
+  (init-sql-dbs)
+  (->> (or (seq arguments) ["test"])
+       (mapcat find-tests)
+       (filter (match-strategy? (:strategy options)))
+       run-tests options))
+
 (defn eftest
   [& args]
-  (init-sql-dbs)
-  (run-tests (find-tests (or (seq args) "test"))
-             {:multithread? :namespaces
-              :capture-output? false}))
+  (let [parsed (parse-opts args eftest-options)]
+    (if (show-help? parsed)
+      (write-help parsed)
+      (eftest* parsed))))
