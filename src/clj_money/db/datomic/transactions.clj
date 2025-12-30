b0a03106622384d1546663a241e54398b9717c49
Doug Knight <dougbknight@gmail.com>
2025-12-30 12:52:55 -0600

    deconstruct transactions for removed items

--- src/clj_money/db/datomic/transactions.clj
+++ src/clj_money/db/datomic/transactions.clj
@@ -1,5 +1,7 @@
 (ns clj-money.db.datomic.transactions
-  (:require [clj-money.db.datomic :as datomic]
+  (:require [clojure.pprint :refer [pprint]]
+            [clj-money.db.datomic :as datomic]
+            [clj-money.entities :as entities]
             [clj-money.util :as util]))
 
 (defn- prepare-item
@@ -21,3 +23,31 @@
   (update-in trx
              [:transaction/items]
              #(map prepare-item %)))
+
+(defn- removed
+  [before after]
+  (let [after-ids (->> after
+                       (map :id)
+                       set)]
+    (remove (comp after-ids :id)
+            before)))
+
+(defn- item-redactions
+  [trx before]
+  (mapcat (fn [item]
+            [[:db/retractEntity (:id item)]
+             [:db/retractEntity (-> item
+                                    :transaction-item/debit-item
+                                    :id)]
+             [:db/retractEntity (-> item
+                                    :transaction-item/credit-item
+                                    :id)]])
+          (removed (:transaction/items before)
+                   (:transaction/items trx))))
+
+(defmethod datomic/deconstruct :transaction
+  [trx]
+  (if-let [before (::entities/before (meta trx))]
+    (cons trx
+          (item-redactions trx before))
+    [trx]))
