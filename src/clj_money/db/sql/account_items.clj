ed2756c5b4ea2769cdc552caab5647043a5f7b93
Doug Knight <dougbknight@gmail.com>
2026-01-10 15:13:21 -0600

    remove dead code

--- src/clj_money/db/sql/account_items.clj
+++ src/clj_money/db/sql/account_items.clj
@@ -1,13 +1,6 @@
 (ns clj-money.db.sql.account-items
-  (:require [clojure.set :refer [rename-keys]]
-            [clj-money.db.sql :as sql]))
-
-(defmethod sql/before-save :account-item
-  [item]
-  (rename-keys item {:transaction/transaction-date :account-item/transaction-date}))
+  (:require [clj-money.db.sql :as sql]))
 
 (defmethod sql/after-read :account-item
   [item]
-  (-> item
-      (update-in [:account-item/action] keyword)
-      (rename-keys {:account-item/transaction-date :transaction/transaction-date})))
+  (update-in item [:account-item/action] keyword))
--- src/clj_money/db/sql/transaction_items.clj
+++ src/clj_money/db/sql/transaction_items.clj
@@ -1,8 +1,5 @@
 (ns clj-money.db.sql.transaction-items
   (:require [clojure.pprint :refer [pprint]]
-            [clojure.set :refer [rename-keys]]
-            [java-time.api :as t]
-            [dgknght.app-lib.core :refer [update-in-if]]
             [clj-money.db.sql :as sql]
             [clj-money.util :refer [temp-id?]]))
 
@@ -13,15 +10,3 @@
   (cond-> item
     (temp-id? transaction-id)
     (update-in [:transaction-item/transaction-id] id-map)))
-
-(defmethod sql/before-save :transaction-item
-  [item]
-  (-> item
-      (update-in-if [:transaction-item/action] name)
-      (update-in [:transaction-item/index] (fnil identity (rand-int 1000)))))
-
-(defmethod sql/after-read :transaction-item
-  [item]
-  (-> item
-      (rename-keys {:transaction-item/transaction-date :transaction/transaction-date})
-      (update-in-if [:transaction/transaction-date] t/local-date)))
