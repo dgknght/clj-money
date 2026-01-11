2d344c84323fa9937f298d5e3f1fdef975690ffd
Doug Knight <dougbknight@gmail.com>
2026-01-11 10:13:15 -0600

    resolve temp ids in account items

--- src/clj_money/db/sql/account_items.clj
+++ src/clj_money/db/sql/account_items.clj
@@ -1,6 +1,11 @@
 (ns clj-money.db.sql.account-items
-  (:require [clj-money.db.sql :as sql]))
+  (:require [dgknght.app-lib.core :refer [update-in-if]]
+            [clj-money.db.sql :as sql]))
 
 (defmethod sql/after-read :account-item
   [item]
   (update-in item [:account-item/action] keyword))
+
+(defmethod sql/resolve-temp-ids :account-item
+  [item id-map]
+  (update-in-if item [:account-item/reconciliation-id] id-map))
