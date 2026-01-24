6ac55f29c015088a3d4d47d04411e5e96cfcedfd
Doug Knight <dougbknight@gmail.com>
2026-01-24 12:00:13 -0600

    fix account-item temp id resolution

--- src/clj_money/db/sql/account_items.clj
+++ src/clj_money/db/sql/account_items.clj
@@ -1,5 +1,7 @@
 (ns clj-money.db.sql.account-items
-  (:require [dgknght.app-lib.core :refer [update-in-if]]
+  (:require [clojure.pprint :refer [pprint]]
+            [dgknght.app-lib.core :refer [update-in-if]]
+            [clj-money.util :refer [temp-id?]]
             [clj-money.db.sql :as sql]))
 
 (defmethod sql/after-read :account-item
@@ -7,5 +9,10 @@
   (update-in item [:account-item/action] keyword))
 
 (defmethod sql/resolve-temp-ids :account-item
-  [item id-map]
-  (update-in-if item [:account-item/reconciliation-id] id-map))
+  [{:as item :account-item/keys [reconciliation-id account-id]} id-map]
+  (cond-> item
+    (temp-id? reconciliation-id)
+    (update-in [:account-item/reconciliation-id] id-map)
+
+    (temp-id? account-id)
+    (update-in [:account-item/account-id] id-map)))
