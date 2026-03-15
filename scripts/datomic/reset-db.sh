6ffd2c73878cbd6e73efa72142b26abfdb8184a9
Doug Knight <dougbknight@gmail.com>
2026-03-15 11:02:37 -0500

    refine datomic reset script

--- scripts/datomic/reset-db.sh
+++ scripts/datomic/reset-db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-podman-compose --profile datomic-peer stop datomic-transactor && \
+podman-compose --profile datomic-peer stop datomic-console datomic-transactor && \
   dropdb --if-exists datomic && \
   . "$(dirname "$0")/init.sh" && \
-  podman-compose --profile datomic-peer start datomic-transactor && \
+  podman-compose --profile datomic-peer start datomic-transactor datomic-console && \
   sleep 20 && \
   lein datomic-schema
