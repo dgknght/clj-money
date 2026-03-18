f24df63f93b470e640da4a7ca77c936005db6863
Doug Knight <dougbknight@gmail.com>
2026-03-18 15:32:44 -0500

    add transaction-item spec for print-entities

--- test/clj_money/db/datomic/helpers.clj
+++ test/clj_money/db/datomic/helpers.clj
@@ -1,20 +1,56 @@
 (ns clj-money.db.datomic.helpers
   (:require [clojure.pprint :refer [pprint]]
+            [clj-money.dates :as dates]
             [clj-money.db.datomic :refer [q]]))
 
 (def ^:private specs
-  {:account {:query '[:find ?a ?name ?quantity ?value
+  {:account {:query '[:find ?a ?name ?commodity ?quantity ?value
                       :where [?a :account/name ?name]
                              [?a :account/quantity ?quantity]
-                             [?a :account/value ?value]]
-             :headers ["ID" "Name" "Quantity" "Value"]
-             :col-widths [16 20 10 10]}
+                             [?a :account/value ?value]
+                             [?a :account/commodity ?c]
+                             [?c :commodity/symbol ?commodity]]
+             :headers ["ID" "Name" "Commodity" "Quantity" "Value"]
+             :col-widths [16 20 10 10 10]
+             :sort-fn second}
    :commodity {:query '[:find ?c ?name ?symbol ?type
                         :where [?c :commodity/name ?name]
                                [?c :commodity/symbol ?symbol]
                                [?c :commodity/type ?type]]
                :headers["ID" "Name" "Symbol" "Type"]
-               :col-widths [16 20 10 10]}})
+               :col-widths [16 20 10 10]
+               :sort-fn second}
+   :transaction-item {:query '[:find ?i ?date ?index ?action ?parent ?account ?quantity ?balance
+                               :where [?i :transaction-item/index ?index]
+                                      [?i :transaction-item/quantity ?quantity]
+                                      [?i :transaction-item/action ?action]
+                                      [?i :transaction-item/balance ?balance]
+                                      [?i :transaction-item/account ?a]
+                                      [?a :account/name ?account]
+                                      [?t :transaction/items ?i]
+                                      [?t :transaction/transaction-date ?date]
+                                      (or-join [?a ?parent]
+                                               (and [?a :account/parent ?p]
+                                                    [?p :account/name ?parent])
+                                               (and [(missing? $ ?a :account/parent)]
+                                                    [(ground "") ?parent]))]
+                      :headers ["ID"
+                                "Date"
+                                "Index"
+                                "Action"
+                                "Parent"
+                                "Account"
+                                "Quantity"
+                                "Balance"]
+                      :col-widths [16 10 6 7 20 20 19 19]
+                      :sort-fn (juxt #(nth % 4)
+                                     #(nth % 5)
+                                     #(nth % 2))
+                      :transform (fn [r]
+                                   (update-in r
+                                              [1]
+                                              (comp dates/format-local-date
+                                                    dates/->local-date)))}})
 
 (defn- print-vals
   [vals]
@@ -44,9 +80,14 @@
   (assert (specs entity-type) (str "No spec found for entity type:" entity-type))
   (println "")
   (println (name entity-type))
-  (let [{:keys [col-widths headers query]} (specs entity-type)]
+  (let [{:keys [col-widths
+                headers
+                query
+                sort-fn
+                transform]
+         :or {transform identity}} (specs entity-type)]
     (print-row headers col-widths)
     (print-separator col-widths)
-    (doseq [row (sort-by second (q query))]
+    (doseq [row (->> (q query) (map transform) (sort-by sort-fn))]
       (print-row row col-widths)))
   (println ""))
