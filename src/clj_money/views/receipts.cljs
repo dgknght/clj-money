21f34893854b3d87f599c893b21c39569c5acf0b
Doug Knight <dougbknight@gmail.com>
2026-04-25 10:47:26 -0500

    avoid unnecessary bilateral rount trip

--- src/clj_money/views/receipts.cljs
+++ src/clj_money/views/receipts.cljs
@@ -23,7 +23,6 @@
                                      -busy]]
             [clj-money.accounts :refer [find-by-path]]
             [clj-money.receipts :as receipts]
-            [clj-money.transactions :refer [->unilateral]]
             [clj-money.api.transactions :as trn]))
 
 (defn- new-receipt
