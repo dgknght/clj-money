c2cbbf6236d3938d0b8e373e6ea901487bd45452
Doug Knight <dougbknight@gmail.com>
2026-05-07 14:04:58 -0500

    api returns just the created entity now

--- src/clj_money/api/attachments.cljs
+++ src/clj_money/api/attachments.cljs
@@ -24,10 +24,7 @@
                           (add-error-handler "Unable to create the attachment: %s")
                           (assoc :oauth-token (:auth-token @app-state))))]
     (a/go
-      ; TODO: This is returning a sequence because of the propagation
-      ; Ideally, we'd just return the attachment immediately and let the
-      ; propagation happen in the background
-      (let [[{:as res :attachment/keys [image]}] (a/<! ch)]
+      (let [{:as res :attachment/keys [image]} (a/<! ch)]
         (callback)
         (when image
           (on-success res))))))
