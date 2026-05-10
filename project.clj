d16607788b6783b8114c9c4296e3b244e33cc50e
Doug Knight <dougbknight@gmail.com>
2026-05-10 16:45:59 -0500

    try repo mirrors

--- project.clj
+++ project.clj
@@ -5,6 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["sonatype" {:url "https://oss.sonatype.org/content/repositories/releases"}]
                  ["sonatype-snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
+  :mirrors {"sonatype" {:name "sonatype"
+                        :url "https://oss.sonatype.org/content/repositories/releases"}
+            "sonatype-snapshots" {:name "sonatype-snapshots"
+                                  :url "https://oss.sonatype.org/content/repositories/snapshots"}}
   :managed-dependencies [[clj-time "0.15.2"]
                          [ring/ring-core "1.12.2"]
                          [commons-io "2.16.1"]
